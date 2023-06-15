7dd147d754e3bc0d084c1338513506f55d0d75e2
5uhwann <5uhwann27@gmail.com>
2023-06-15 17:23:10 +0900

    init: init project

---
+++ src/main/java/ddingdong/ddingdongBE/DdingdongBeApplication.java
@@ -0,0 +1,13 @@
+package ddingdong.ddingdongBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdingdongBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdingdongBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ddingdong/ddingdongBE/DdingdongBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ddingdong.ddingdongBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdingdongBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
