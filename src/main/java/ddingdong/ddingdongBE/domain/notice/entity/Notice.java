b4fb1a6d618a2e2e2a7009ee014600d2eaa36f6c
5uhwann <5uhwann27@gmail.com>
2023-06-17 15:12:09 +0900

    feat: Notice 엔티티 작성

---
+++ src/main/java/ddingdong/ddingdongBE/domain/notice/entity/Notice.java
@@ -0,0 +1,41 @@
+package ddingdong.ddingdongBE.domain.notice.entity;
+
+import ddingdong.ddingdongBE.common.BaseEntity;
+import ddingdong.ddingdongBE.domain.user.entity.User;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notice extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String title;
+
+    private String content;
+
+    @Builder
+    public Notice(User user, String title, String content) {
+        this.user = user;
+        this.title = title;
+        this.content = content;
+    }
+
+}
