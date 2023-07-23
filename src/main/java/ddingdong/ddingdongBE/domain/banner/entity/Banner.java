e97b5936a8b87f307644cdf8544810917e758d1d
5uhwann <5uhwann27@gmail.com>
2023-07-23 16:05:40 +0900

    refactor: Banner 엔티티 수정

--- src/main/java/ddingdong/ddingdongBE/domain/banner/entity/Banner.java
+++ src/main/java/ddingdong/ddingdongBE/domain/banner/entity/Banner.java
@@ -3,11 +3,12 @@ package ddingdong.ddingdongBE.domain.banner.entity;
 import ddingdong.ddingdongBE.common.BaseEntity;
 import ddingdong.ddingdongBE.domain.banner.controller.dto.request.UpdateBannerRequest;
 import ddingdong.ddingdongBE.domain.user.entity.User;
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -23,8 +24,8 @@ public class Banner extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @Column(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     private String title;
