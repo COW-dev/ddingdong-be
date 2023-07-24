1cb4234c47702702e2077f5b8632e52a0f6fa8fd
5uhwann <5uhwann27@gmail.com>
2023-07-24 22:07:09 +0900

    feat: Banner 전체 조회 controller 연결

---
+++ src/main/java/ddingdong/ddingdongBE/domain/banner/controller/UserBannerController.java
@@ -0,0 +1,23 @@
+package ddingdong.ddingdongBE.domain.banner.controller;
+
+import ddingdong.ddingdongBE.domain.banner.controller.dto.response.BannerResponse;
+import ddingdong.ddingdongBE.domain.banner.service.BannerService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/banners")
+@RequiredArgsConstructor
+public class UserBannerController {
+
+    private final BannerService bannerService;
+
+    @GetMapping
+    public List<BannerResponse> getBanners() {
+        return bannerService.getAllBanners();
+    }
+
+}
