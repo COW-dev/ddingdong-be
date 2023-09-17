504bde5ade14935f0a230564c8f77bb427053314
wonjunYou <youwjune@gmail.com>
2023-09-17 12:54:39 +0900

    fix: 동아리 멤버 테이블 초기화 버그 수정

--- src/main/java/ddingdong/ddingdongBE/domain/club/repository/ClubMemberRepository.java
+++ src/main/java/ddingdong/ddingdongBE/domain/club/repository/ClubMemberRepository.java
@@ -1,8 +1,19 @@
 package ddingdong.ddingdongBE.domain.club.repository;
 
 import ddingdong.ddingdongBE.domain.club.entity.ClubMember;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
+    List<ClubMember> findClubMembersByClubId(Long clubId);
 
+    @Modifying
+    @Transactional
+    @Query("delete from ClubMember c where c.id in :memberIds")
+    void deleteAllById(List<Long> memberIds);
 }
