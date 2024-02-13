065efbfa6e46e587f9c6eba66b1ab31f8ad306f8
wonjunYou <youwjune@gmail.com>
2024-02-13 14:00:21 +0900

    chore : qr uri에 depth 추가

--- src/main/java/ddingdong/ddingdongBE/common/utils/EventQrGenerator.java
+++ src/main/java/ddingdong/ddingdongBE/common/utils/EventQrGenerator.java
@@ -9,7 +9,7 @@ public class EventQrGenerator {
     public static final String URI_PREFIX = "https://chart.apis.google.com/chart?cht=qr&chs=250x250&chl=";
 
     public EventQrResponse generateQrCodeUri(String studentName, String studentNumber) {
-        String uri = URI_PREFIX + "https://ddingdong.club/event?studentName=" + studentName + "%26" + "studentNumber=" + studentNumber;
+        String uri = URI_PREFIX + "https://ddingdong.club/event/code?studentName=" + studentName + "%26" + "studentNumber=" + studentNumber;
 
         return EventQrResponse.of(uri);
     }
