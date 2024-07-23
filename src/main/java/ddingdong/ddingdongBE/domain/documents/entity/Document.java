4b386c8fb255615a2ee03077b30cd9305078bf2f
5uhwann <5uhwann27@gmail.com>
2024-07-23 17:45:44 +0900

    feat: document entity 정의

---
+++ src/main/java/ddingdong/ddingdongBE/domain/documents/entity/Document.java
@@ -0,0 +1,28 @@
+package ddingdong.ddingdongBE.domain.documents.entity;
+
+import ddingdong.ddingdongBE.common.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Document extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 1024)
+    private String content;
+
+}
