90c24fdabbc1dcab25f8eef0b39337c215278b8b
5uhwann <5uhwann27@gmail.com>
2024-08-14 14:45:30 +0900

    refactor: server time_zone 변경(Asia/Seoul)

--- src/main/java/ddingdong/ddingdongBE/DdingdongBeApplication.java
+++ src/main/java/ddingdong/ddingdongBE/DdingdongBeApplication.java
@@ -1,10 +1,17 @@
 package ddingdong.ddingdongBE;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DdingdongBeApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DdingdongBeApplication.class, args);
