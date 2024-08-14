abe33ef35ead7efdbb54c671b577c1fdcc0bf4a0
koseonje <kosunje1344@naver.com>
2024-08-14 16:39:12 +0900

    test : 컨트롤러 통합테스트용 support클래스 생성

---
+++ src/test/java/ddingdong/ddingdongBE/common/support/WebApiIntegrationTestSupport.java
@@ -0,0 +1,25 @@
+package ddingdong.ddingdongBE.common.support;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+// 컨트롤러 단 통합테스트용
+@SpringBootTest
+@AutoConfigureMockMvc
+public abstract class WebApiIntegrationTestSupport extends TestContainerSupport {
+
+  @Autowired
+  protected MockMvc mockMvc;
+
+  @Autowired
+  protected ObjectMapper objectMapper;
+
+  protected String toJson(Object object) throws JsonProcessingException {
+    return objectMapper.writeValueAsString(object);
+  }
+
+}
