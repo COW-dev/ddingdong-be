236aa39bc1f0518e9ceedf1a1fd3f5d6df38ae5c
koseonje <kosunje1344@naver.com>
2024-08-14 16:38:21 +0900

    test : 테이블 데이터 초기화 작업을 위한 클래스 생성

---
+++ src/test/java/ddingdong/ddingdongBE/common/support/DataInitializer.java
@@ -0,0 +1,49 @@
+package ddingdong.ddingdongBE.common.support;
+
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Profile("test")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Component
+public class DataInitializer {
+
+  private static final String OFF_FOREIGN_CONSTRAINTS = "SET foreign_key_checks = false";
+  private static final String ON_FOREIGN_CONSTRAINTS = "SET foreign_key_checks = true";
+  private static final String TRUNCATE_SQL_FORMAT = "TRUNCATE %s";
+
+  private static final List<String> truncationDMLs = new ArrayList<>();
+
+  @PersistenceContext
+  private EntityManager em;
+
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  public void deleteAll() {
+    if (truncationDMLs.isEmpty()) {
+      init();
+    }
+
+    em.createNativeQuery(OFF_FOREIGN_CONSTRAINTS).executeUpdate();
+    truncationDMLs.stream()
+        .map(em::createNativeQuery)
+        .forEach(Query::executeUpdate);
+    em.createNativeQuery(ON_FOREIGN_CONSTRAINTS).executeUpdate();
+  }
+
+  private void init() {
+    final List<String> tableNames = em.createNativeQuery("SHOW TABLES ").getResultList();
+
+    tableNames.stream()
+        .map(tableName -> String.format(TRUNCATE_SQL_FORMAT, tableName))
+        .forEach(truncationDMLs::add);
+  }
+}
