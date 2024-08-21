15ff069f38b8ef6a25d3a111fa7f3040fca34434
5uhwann <5uhwann27@gmail.com>
2024-08-21 21:06:57 +0900

    refactor: ExceptionHandling 수정

--- src/main/java/ddingdong/ddingdongBE/common/exception/CustomException.java
+++ src/main/java/ddingdong/ddingdongBE/common/exception/CustomException.java
@@ -2,9 +2,11 @@ package ddingdong.ddingdongBE.common.exception;
 
 abstract class CustomException extends RuntimeException {
     String message;
+    int errorCode;
 
-    public CustomException(String message) {
+    public CustomException(String message, int errorCode) {
         this.message = message;
+        this.errorCode = errorCode;
     }
 
 }
--- src/main/java/ddingdong/ddingdongBE/common/exception/ErrorResponse.java
+++ src/main/java/ddingdong/ddingdongBE/common/exception/ErrorResponse.java
@@ -3,13 +3,9 @@ package ddingdong.ddingdongBE.common.exception;
 import java.time.LocalDateTime;
 
 public record ErrorResponse(
-    int status,
-    String message,
-    LocalDateTime timestamp
+        int status,
+        String message,
+        LocalDateTime timestamp
 ) {
-
-    public ErrorResponse(int status, String message) {
-        this(status, message, LocalDateTime.now());
-    }
 
 }
