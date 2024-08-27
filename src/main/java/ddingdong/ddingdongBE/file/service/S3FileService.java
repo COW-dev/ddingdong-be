dabee51e1d5aea1e4d19f3ef05162e95596368eb
5uhwann <5uhwann27@gmail.com>
2024-08-27 18:24:05 +0900

    feat: presignedUrl 발급 에러 명세 추가

--- src/main/java/ddingdong/ddingdongBE/file/service/S3FileService.java
+++ src/main/java/ddingdong/ddingdongBE/file/service/S3FileService.java
@@ -1,15 +1,21 @@
 package ddingdong.ddingdongBE.file.service;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import ddingdong.ddingdongBE.common.exception.AwsException.AwsClient;
+import ddingdong.ddingdongBE.common.exception.AwsException.AwsService;
 import java.net.URL;
 import java.util.Date;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class S3FileService {
 
@@ -28,12 +34,20 @@ public class S3FileService {
         long expTimeMillis = expiration.getTime();
         expTimeMillis += 1000 * 60 * 5;
         expiration.setTime(expTimeMillis);
+        try {
+            GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucketName, path)
+                    .withMethod(HttpMethod.PUT)
+                    .withExpiration(expiration);
 
-        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucketName, path)
-                .withMethod(HttpMethod.PUT)
-                .withExpiration(expiration);
+            return amazonS3Client.generatePresignedUrl(generatePresignedUrlRequest);
+        } catch (AmazonServiceException e) {
+            log.warn("AWS Service Error : {}", e.getMessage());
+            throw new AwsService();
+        } catch (AmazonClientException e) {
+            log.warn("AWS Client Error : {}", e.getMessage());
+            throw new AwsClient();
+        }
 
-        return amazonS3Client.generatePresignedUrl(generatePresignedUrlRequest);
     }
 
     private String createFilePath(String fileName) {
