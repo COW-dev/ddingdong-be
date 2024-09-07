ba145eb5ae20b3011c1e407599846ef05ad6c9dd
5uhwann <5uhwann27@gmail.com>
2024-09-07 11:04:58 +0900

    refactor: flyway 마이그레이션 스크립트 수정

--- src/main/resources/db/migration/V4__Create_feed.sql
+++ src/main/resources/db/migration/V4__Create_feed.sql
@@ -2,7 +2,7 @@ CREATE TABLE feed (
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
         activity_content TEXT NOT NULL,
         thumbnail_url VARCHAR(255) NOT NULL,
-        feed_type TINYINT NOT NULL,
+        feed_type VARCHAR(10) NOT NULL,
         deleted_at DATETIME,
         club_id BIGINT,
         created_at DATETIME NOT NULL,
