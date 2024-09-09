0a39292741cd2918862ed3854ba41c987f654613
wonjunYou <youwjune@gmail.com>
2024-09-10 01:27:36 +0900

    [DEV-58] 에러 모니터링을 위한 Sentry 도입 및 discord webhook 연결

--- src/main/java/ddingdong/ddingdongBE/common/exception/CustomExceptionHandler.java
+++ src/main/java/ddingdong/ddingdongBE/common/exception/CustomExceptionHandler.java
@@ -4,10 +4,15 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+import io.sentry.Sentry;
 import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.NoSuchElementException;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -30,9 +35,29 @@ public class CustomExceptionHandler {
     public ErrorResponse handleSystemException(Throwable exception, HttpServletRequest request) {
         String connectionInfo = createLogConnectionInfo(request);
 
+        String requestMethod = request.getMethod();
+        String requestUrl = request.getRequestURI();
+        String queryString =
+            (request.getQueryString() != null) ? "?" + request.getQueryString() : "";
+        String clientIp =
+            (request.getHeader("X-Forwarded-For") != null) ? request.getHeader("X-Forwarded-For")
+                : request.getRemoteAddr();
+
         loggingApplicationError(connectionInfo
                 + "\n"
                 + "[SYSTEM-ERROR]" + " : " + exception.getMessage());
+
+        Sentry.captureException(exception, scope -> {
+            scope.setExtra("requestMethod", requestMethod);
+            scope.setExtra("requestUrl", requestUrl);
+            scope.setExtra("queryString", queryString);
+            scope.setExtra("clientIp", clientIp);
+
+            String requestBody = getRequestBody(request);
+            if (requestBody != null) {
+                scope.setExtra("requestBody", requestBody);
+            }
+        });
 
         return new ErrorResponse(INTERNAL_SERVER_ERROR.value(), "Internal Sever Error", LocalDateTime.now());
     }
@@ -111,7 +136,6 @@ public class CustomExceptionHandler {
         return ExceptionResponse.of(BAD_REQUEST, e.getMessage());
     }
 
-
     private String createLogConnectionInfo(HttpServletRequest request) {
         String requestMethod = request.getMethod();
         String requestUrl = request.getRequestURI();
@@ -124,5 +148,23 @@ public class CustomExceptionHandler {
 
     private void loggingApplicationError(String applicationLog) {
         log.warn("errorLog = {}", applicationLog);
+    }
+
+    public String getRequestBody(HttpServletRequest request) {
+        try {
+            InputStream inputStream = request.getInputStream();
+            BufferedReader reader = new BufferedReader(
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+
+            return stringBuilder.toString();
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
