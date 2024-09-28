e722307c949a8df889a7a3277c27e9e290189747
5uhwann <106325839+5uhwann@users.noreply.github.com>
2024-09-28 18:57:45 +0900

    [DEV-5]로그 수집을 위한 promtail 설정 (#127)

---
+++ src/main/java/ddingdong/ddingdongBE/common/runner/PromtailDockerComposeRunner.java
@@ -0,0 +1,50 @@
+package ddingdong.ddingdongBE.common.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+@Component
+@Profile("prod")
+@Slf4j
+public class PromtailDockerComposeRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("Running PromtailDockerComposeRunner");
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("docker-compose", "-f", "/var/app/current/promtail/promtail-docker-compose.yml", "up", "-d");
+
+        Process process = processBuilder.start();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                log.info(line);
+            }
+        }
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                log.warn(line);
+            }
+        }
+
+        int exitCode = process.waitFor();
+        if (exitCode == 0) {
+            log.info("Promtail started successfully using Docker Compose.");
+            log.info("promtial is tracking info level log");
+            log.warn("promtial is tracking warn level log");
+            log.error("promtial is tracking error level log");
+        } else {
+            log.error("Failed to start Promtail. Exit code: {}", exitCode);
+        }
+    }
+}
