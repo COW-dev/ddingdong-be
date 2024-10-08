0fdfeb011349ed87e338c26af7f2c4cf3f149eb1
5uhwann <5uhwann27@gmail.com>
2024-10-08 18:37:04 +0900

    refactor: FacadeAdminScoreHistoryService 구현

--- src/main/java/ddingdong/ddingdongBE/domain/scorehistory/api/AdminScoreHistoryApi.java
+++ src/main/java/ddingdong/ddingdongBE/domain/scorehistory/api/AdminScoreHistoryApi.java
@@ -2,7 +2,7 @@ package ddingdong.ddingdongBE.domain.scorehistory.api;
 
 import ddingdong.ddingdongBE.common.exception.ErrorResponse;
 import ddingdong.ddingdongBE.domain.scorehistory.controller.dto.request.CreateScoreHistoryRequest;
-import ddingdong.ddingdongBE.domain.scorehistory.controller.dto.response.ScoreHistoryFilterByClubResponse;
+import ddingdong.ddingdongBE.domain.scorehistory.controller.dto.response.ScoreHistoryListResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
@@ -62,7 +62,7 @@ public interface AdminScoreHistoryApi {
             @ApiResponse(responseCode = "200",
                     description = "점수 변동 내역 목록 조회 성공",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = ScoreHistoryFilterByClubResponse.class))),
+                            schema = @Schema(implementation = ScoreHistoryListResponse.class))),
             @ApiResponse(responseCode = "400",
                     description = "잘못된 요청",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
@@ -83,6 +83,6 @@ public interface AdminScoreHistoryApi {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     @SecurityRequirement(name = "AccessToken")
-    ScoreHistoryFilterByClubResponse findAllScoreHistories(@PathVariable Long clubId);
+    ScoreHistoryListResponse findAllScoreHistories(@PathVariable Long clubId);
 
 }
