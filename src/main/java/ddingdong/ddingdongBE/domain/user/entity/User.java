3f927de7654c2b0b4728c0609c0940dad85aa235
5uhwann <5uhwann27@gmail.com>
2024-10-11 15:35:34 +0900

    refactor: User 엔티티 컬럼 명 수정

--- src/main/java/ddingdong/ddingdongBE/domain/user/entity/User.java
+++ src/main/java/ddingdong/ddingdongBE/domain/user/entity/User.java
@@ -29,8 +29,8 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "userid")
-    private String userId;
+    //TODO: migration script 작성
+    private String authId;
 
     private String password;
 
@@ -43,9 +43,9 @@ public class User extends BaseEntity {
     private LocalDateTime deletedAt;
 
     @Builder
-    public User(Long id, String userId, String password, String name, Role role) {
+    public User(Long id, String authId, String password, String name, Role role) {
         this.id = id;
-        this.userId = userId;
+        this.authId = authId;
         this.password = password;
         this.name = name;
         this.role = role;
