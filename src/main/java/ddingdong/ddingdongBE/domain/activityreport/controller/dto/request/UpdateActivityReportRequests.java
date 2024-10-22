8dc5bbcd4d6c8f0b9e009d7f2d1f45db4537225b
koseonje <kosunje1344@naver.com>
2024-10-22 22:38:31 +0900

    refactor : 명세 수정

--- src/main/java/ddingdong/ddingdongBE/domain/activityreport/controller/dto/request/UpdateActivityReportRequests.java
+++ src/main/java/ddingdong/ddingdongBE/domain/activityreport/controller/dto/request/UpdateActivityReportRequests.java
@@ -3,9 +3,9 @@ package ddingdong.ddingdongBE.domain.activityreport.controller.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
-@Schema(name = "CreateActivityReportRequests", description = "활동보고서 수정 요청")
+@Schema(name = "UpdateActivityReportRequests", description = "활동보고서 수정 요청")
 public record UpdateActivityReportRequests(
-    @Schema(description = "요청된 활동보고서들", implementation = CreateActivityReportRequest.class)
+    @Schema(description = "요청된 활동보고서들", implementation = UpdateActivityReportRequest.class)
     List<UpdateActivityReportRequest> activityReportRequests
 ) {
 }
