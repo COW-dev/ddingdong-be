af5b36c99a5ee4186a8647025fe2667862788b94
5uhwann <5uhwann27@gmail.com>
2024-10-25 17:22:20 +0900

    refactor: 동아리원 명단 조회 구현

--- src/main/java/ddingdong/ddingdongBE/domain/clubmember/service/FacadeCentralClubMemberService.java
+++ src/main/java/ddingdong/ddingdongBE/domain/clubmember/service/FacadeCentralClubMemberService.java
@@ -2,10 +2,14 @@ package ddingdong.ddingdongBE.domain.clubmember.service;
 
 import ddingdong.ddingdongBE.domain.clubmember.service.dto.command.UpdateClubMemberCommand;
 import ddingdong.ddingdongBE.domain.clubmember.service.dto.command.UpdateClubMemberListCommand;
+import ddingdong.ddingdongBE.domain.clubmember.service.dto.query.CentralClubMemberListQuery;
+import java.util.List;
 
-public interface FacadeClubMemberService {
+public interface FacadeCentralClubMemberService {
 
     byte[] getClubMemberListFile(Long userId);
+
+    List<CentralClubMemberListQuery> getAllMyClubMember(Long userId);
 
     void updateMemberList(UpdateClubMemberListCommand command);
 
