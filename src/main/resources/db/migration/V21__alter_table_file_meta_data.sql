b6246bd667911117c65ec5b6bf947b6b4218b175
5uhwann <5uhwann27@gmail.com>
2024-10-30 00:54:54 +0900

    chore: migration script 통합

--- src/main/resources/db/migration/V21__alter_table_file_meta_data.sql
+++ src/main/resources/db/migration/V21__alter_table_file_meta_data.sql
@@ -1,31 +1,15 @@
 ALTER TABLE file_meta_data
-    ADD entity_id BIGINT NULL;
+    DROP COLUMN file_id,
+    ADD entity_id BIGINT NULL,
+    ADD domain_type VARCHAR(255) NULL,
+    ADD id BINARY(16) NULL,
+    ADD file_key VARCHAR(255) NOT NULL,
+    ADD file_name VARCHAR(255) NOT NULL,
+    ADD file_status VARCHAR(255) NOT NULL,
+    MODIFY file_category VARCHAR(255) NULL;
 
-ALTER TABLE file_meta_data
-    ADD domain_type VARCHAR(255) NULL;
-
-ALTER TABLE file_meta_data
-    ADD file_status VARCHAR(255) NULL;
-
-ALTER TABLE file_meta_data
-    ADD id BINARY(16) NULL;
-
-ALTER TABLE file_meta_data
-    ADD `key` VARCHAR(255) NULL;
-
-ALTER TABLE file_meta_data
-    ADD file_name VARCHAR(255) NULL;
-
-ALTER TABLE file_meta_data
-    MODIFY file_status VARCHAR (255) NOT NULL;
-
-ALTER TABLE file_meta_data
-    MODIFY `key` VARCHAR (255) NOT NULL;
-
-CREATE INDEX IDX_file_meta_data_domain_type_entity_id_file_status ON file_meta_data (domain_type, entity_id, file_status);
-
-ALTER TABLE file_meta_data
-    DROP COLUMN file_id;
+CREATE INDEX IDX_file_meta_data_domain_type_entity_id_file_status
+    ON file_meta_data (domain_type, entity_id, file_status);
 
 ALTER TABLE file_meta_data
     ADD PRIMARY KEY (id);
