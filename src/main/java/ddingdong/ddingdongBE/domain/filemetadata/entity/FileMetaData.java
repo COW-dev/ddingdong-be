f75d966ec3f91ae8b05123bf2f81ef61a277a2c2
5uhwann <5uhwann27@gmail.com>
2024-10-31 00:34:31 +0900

    refactor: 불필요 임포트 제거

--- src/main/java/ddingdong/ddingdongBE/domain/filemetadata/entity/FileMetaData.java
+++ src/main/java/ddingdong/ddingdongBE/domain/filemetadata/entity/FileMetaData.java
@@ -1,6 +1,5 @@
 package ddingdong.ddingdongBE.domain.filemetadata.entity;
 
-import com.github.f4b6a3.uuid.UuidCreator;
 import ddingdong.ddingdongBE.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
