7cc2c0ad388217902c107303caf9e063f68d1a86
koseonje <127813439+KoSeonJe@users.noreply.github.com>
2024-11-03 00:56:56 +0900

    [DDING-000] Document Test 에러 수정 (#169)

--- src/test/java/ddingdong/ddingdongBE/domain/documents/controller/DocumentControllerUnitTest.java
+++ src/test/java/ddingdong/ddingdongBE/domain/documents/controller/DocumentControllerUnitTest.java
@@ -1,5 +1,6 @@
 package ddingdong.ddingdongBE.domain.documents.controller;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -12,6 +13,9 @@ import ddingdong.ddingdongBE.common.support.WebApiUnitTestSupport;
 import ddingdong.ddingdongBE.common.support.WithMockAuthenticatedUser;
 import ddingdong.ddingdongBE.domain.documents.entity.Document;
 import ddingdong.ddingdongBE.domain.documents.service.dto.query.DocumentListPagingQuery;
+import ddingdong.ddingdongBE.domain.documents.service.dto.query.DocumentQuery;
+import ddingdong.ddingdongBE.file.service.dto.query.UploadedFileUrlAndNameQuery;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -29,42 +33,43 @@ class DocumentControllerUnitTest extends WebApiUnitTestSupport {
             Document.builder().id(1L).title("A").createdAt(LocalDateTime.now()).build(),
             Document.builder().id(2L).title("B").createdAt(LocalDateTime.now()).build()
         );
-        int totalPageCount = 10;
+        Long totalPageCount = 10L;
         DocumentListPagingQuery queries = DocumentListPagingQuery.of(documents, totalPageCount);
         when(facadeDocumentServiceImpl.getDocumentList(any())).thenReturn(queries);
 
         //when //then
         mockMvc.perform(get("/server/documents?page=1&limit=10")
-                        .with(csrf()))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").value(1L))
-                .andExpect(jsonPath("$[0].title").value("A"))
-                .andExpect(jsonPath("$[1].title").value("B"));
+                .with(csrf()))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.documents[0].id").value(1L))
+            .andExpect(jsonPath("$.documents[0].title").value("A"))
+            .andExpect(jsonPath("$.documents[1].title").value("B"));
     }
 
-//    @WithMockAuthenticatedUser
-//    @DisplayName("documents 상세조회 요청을 수행한다.")
-//    @Test
-//    void getDocument() throws Exception {
-//        //given
-//        List<UploadedFileUrlQuery> filurls = List.of(new UploadedFileUrlQuery("originUrl1","cdnUrl1"),
-//            new UploadedFileUrlQuery("originUrl2","cdnUrl2"));
-//        DocumentQuery query = DocumentQuery.builder()
-//            .title("title")
-//            .fileUrls(filurls)
-//            .createdAt(LocalDate.now()).build();
-//        Long documentId = 1L;
-//        when(facadeDocumentService.getDocument(documentId)).thenReturn(query);
-//
-//        //when //then
-//        mockMvc.perform(get("/server/documents/{documentId}", 1L)
-//                        .with(csrf()))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.title").value("title"))
-//                .andExpect(jsonPath("$.fileUrls", hasSize(filurls.size())))
-//                .andExpect(jsonPath("$.fileUrls[0].originUrl").value("originUrl1"))
-//                .andExpect(jsonPath("$.fileUrls[0].cdnUrl").value("cdnUrl1"));
-//    }
+    @WithMockAuthenticatedUser
+    @DisplayName("documents 상세조회 요청을 수행한다.")
+    @Test
+    void getDocument() throws Exception {
+        //given
+        List<UploadedFileUrlAndNameQuery> files = List.of(
+            new UploadedFileUrlAndNameQuery("1","name","originUrl1","cdnUrl1"),
+            new UploadedFileUrlAndNameQuery("2","name2","originUrl2","cdnUrl2"));
+        DocumentQuery query = DocumentQuery.builder()
+            .title("title")
+            .fileInfoQueries(files)
+            .createdAt(LocalDate.now()).build();
+        Long documentId = 1L;
+        when(facadeDocumentServiceImpl.getDocument(documentId)).thenReturn(query);
+
+        //when //then
+        mockMvc.perform(get("/server/documents/{documentId}", 1L)
+                .with(csrf()))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.title").value("title"))
+            .andExpect(jsonPath("$.files", hasSize(files.size())))
+            .andExpect(jsonPath("$.files[0].originUrl").value("originUrl1"))
+            .andExpect(jsonPath("$.files[0].cdnUrl").value("cdnUrl1"));
+    }
 }
