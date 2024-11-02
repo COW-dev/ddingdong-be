32a30e88cf7a08e53afdf7c72669eb91cdde6e1d
koseonje <127813439+KoSeonJe@users.noreply.github.com>
2024-11-02 21:31:58 +0900

    refactor : 의존성 방법 수정

--- src/main/java/ddingdong/ddingdongBE/domain/documents/controller/AdminDocumentController.java
+++ src/main/java/ddingdong/ddingdongBE/domain/documents/controller/AdminDocumentController.java
@@ -4,7 +4,7 @@ import ddingdong.ddingdongBE.auth.PrincipalDetails;
 import ddingdong.ddingdongBE.domain.documents.api.AdminDocumentApi;
 import ddingdong.ddingdongBE.domain.documents.controller.dto.request.CreateDocumentRequest;
 import ddingdong.ddingdongBE.domain.documents.controller.dto.request.UpdateDocumentRequest;
-import ddingdong.ddingdongBE.domain.documents.service.FacadeAdminDocumentServiceImpl;
+import ddingdong.ddingdongBE.domain.documents.service.FacadeAdminDocumentService;
 import ddingdong.ddingdongBE.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,24 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AdminDocumentController implements AdminDocumentApi {
 
-    private final FacadeAdminDocumentServiceImpl facadeAdminDocumentServiceImpl;
+    private final FacadeAdminDocumentService facadeAdminDocumentService;
 
     public void createDocument(
         @AuthenticationPrincipal PrincipalDetails principalDetails,
         CreateDocumentRequest createDocumentRequest
     ) {
         User admin = principalDetails.getUser();
-        facadeAdminDocumentServiceImpl.create(createDocumentRequest.toCommand(admin));
+        facadeAdminDocumentService.create(createDocumentRequest.toCommand(admin));
     }
 
     public void updateDocument(
         @PathVariable Long documentId,
         UpdateDocumentRequest updateDocumentRequest
     ) {
-        facadeAdminDocumentServiceImpl.update(updateDocumentRequest.toCommand(documentId));
+        facadeAdminDocumentService.update(updateDocumentRequest.toCommand(documentId));
     }
 
     public void deleteDocument(@PathVariable Long documentId) {
-        facadeAdminDocumentServiceImpl.delete(documentId);
+        facadeAdminDocumentService.delete(documentId);
     }
 }
--- src/main/java/ddingdong/ddingdongBE/domain/documents/controller/DocumentController.java
+++ src/main/java/ddingdong/ddingdongBE/domain/documents/controller/DocumentController.java
@@ -4,7 +4,7 @@ import ddingdong.ddingdongBE.domain.documents.api.DocumentApi;
 import ddingdong.ddingdongBE.domain.documents.controller.dto.request.GetDocumentPagingRequest;
 import ddingdong.ddingdongBE.domain.documents.controller.dto.response.DocumentListResponse;
 import ddingdong.ddingdongBE.domain.documents.controller.dto.response.DocumentResponse;
-import ddingdong.ddingdongBE.domain.documents.service.FacadeDocumentServiceImpl;
+import ddingdong.ddingdongBE.domain.documents.service.FacadeDocumentService;
 import ddingdong.ddingdongBE.domain.documents.service.dto.query.DocumentListPagingQuery;
 import ddingdong.ddingdongBE.domain.documents.service.dto.query.DocumentQuery;
 import lombok.RequiredArgsConstructor;
@@ -15,17 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class DocumentController implements DocumentApi {
 
-    private final FacadeDocumentServiceImpl facadeDocumentServiceImpl;
+    private final FacadeDocumentService facadeDocumentService;
 
     @Override
     public DocumentListResponse getDocumentList(GetDocumentPagingRequest request) {
-        DocumentListPagingQuery query = facadeDocumentServiceImpl.getDocumentList(request.toCommand());
+        DocumentListPagingQuery query = facadeDocumentService.getDocumentList(request.toCommand());
         return DocumentListResponse.from(query);
     }
 
     @Override
     public DocumentResponse getDocument(@PathVariable Long documentId) {
-        DocumentQuery query = facadeDocumentServiceImpl.getDocument(documentId);
+        DocumentQuery query = facadeDocumentService.getDocument(documentId);
         return DocumentResponse.from(query);
     }
 }
