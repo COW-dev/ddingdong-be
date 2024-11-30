748cf16dba47230a087fcc1651eef7626fda31f7
5uhwann <5uhwann27@gmail.com>
2024-11-30 20:50:26 +0900

    refactor: BannerRepository - 불필요 쿼리 삭제

--- src/main/java/ddingdong/ddingdongBE/domain/banner/repository/BannerRepository.java
+++ src/main/java/ddingdong/ddingdongBE/domain/banner/repository/BannerRepository.java
@@ -1,22 +1,13 @@
 package ddingdong.ddingdongBE.domain.banner.repository;
 
 import ddingdong.ddingdongBE.domain.banner.entity.Banner;
-import ddingdong.ddingdongBE.domain.banner.repository.dto.BannerWithFileMetaData;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BannerRepository extends JpaRepository<Banner, Long> {
 
     List<Banner> findAllByOrderByIdDesc();
-
-    @Query("SELECT new ddingdong.ddingdongBE.domain.banner.repository.dto.BannerWithFileMetaData(" +
-            "b, f) " +
-            "FROM Banner b " +
-            "LEFT JOIN FileMetaData f ON b.id = f.entityId " +
-            "ORDER BY b.id DESC")
-    List<BannerWithFileMetaData> findAllBannersWithFileMetaData();
 
 }
