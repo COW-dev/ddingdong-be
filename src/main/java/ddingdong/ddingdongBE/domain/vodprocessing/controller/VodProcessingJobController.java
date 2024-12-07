8474a0b3b8597811f1940bc0ec6a1f83069bd4c1
5uhwann <5uhwann27@gmail.com>
2024-12-07 16:48:11 +0900

    refactor: VodProcessingJobController swagger 명세 제외

--- src/main/java/ddingdong/ddingdongBE/domain/vodprocessing/controller/VodProcessingJobController.java
+++ src/main/java/ddingdong/ddingdongBE/domain/vodprocessing/controller/VodProcessingJobController.java
@@ -2,10 +2,12 @@ package ddingdong.ddingdongBE.domain.vodprocessing.controller;
 
 import ddingdong.ddingdongBE.domain.vodprocessing.controller.dto.request.CreatePendingVodProcessingJobRequest;
 import ddingdong.ddingdongBE.domain.vodprocessing.service.FacadeVodProcessingJobService;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RestController("/server/internal/trigger/vod-processing-job")
 @RequiredArgsConstructor
 public class VodProcessingJobController {
