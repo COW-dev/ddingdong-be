16f5474811878fa50b5079afa183e9771901ce54
5uhwann <5uhwann27@gmail.com>
2025-01-06 00:31:19 +0900

    refactor: 누락 어노테이션 추가

--- src/main/java/ddingdong/ddingdongBE/domain/vodprocessing/controller/VodProcessingJobController.java
+++ src/main/java/ddingdong/ddingdongBE/domain/vodprocessing/controller/VodProcessingJobController.java
@@ -5,6 +5,7 @@ import ddingdong.ddingdongBE.domain.vodprocessing.service.FacadeVodProcessingJob
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class VodProcessingJobController {
     private final FacadeVodProcessingJobService facadeVodProcessingJobService;
 
     @PostMapping()
-    public void createPending(CreatePendingVodProcessingJobRequest request) {
+    public void createPending(@RequestBody CreatePendingVodProcessingJobRequest request) {
         facadeVodProcessingJobService.create(request.toCommand());
     }
 
