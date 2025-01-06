e1f58e1282d8d9a6cbfc3feacc03536b94f1f956
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-01-06 23:51:00 +0900

    refactor : 사용하지 않는 필드 제거

--- src/main/java/ddingdong/ddingdongBE/domain/feed/service/FacadeClubFeedServiceImpl.java
+++ src/main/java/ddingdong/ddingdongBE/domain/feed/service/FacadeClubFeedServiceImpl.java
@@ -3,11 +3,9 @@ package ddingdong.ddingdongBE.domain.feed.service;
 import ddingdong.ddingdongBE.domain.club.entity.Club;
 import ddingdong.ddingdongBE.domain.club.service.ClubService;
 import ddingdong.ddingdongBE.domain.feed.entity.Feed;
-import ddingdong.ddingdongBE.domain.feed.repository.FeedRepository;
 import ddingdong.ddingdongBE.domain.feed.service.dto.command.CreateFeedCommand;
 import ddingdong.ddingdongBE.domain.filemetadata.entity.DomainType;
 import ddingdong.ddingdongBE.domain.filemetadata.service.FileMetaDataService;
-import ddingdong.ddingdongBE.file.service.S3FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,10 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class FacadeClubFeedServiceImpl implements FacadeClubFeedService{
 
-    private final FeedRepository feedRepository;
     private final ClubService clubService;
     private final FileMetaDataService fileMetaDataService;
-    private final S3FileService s3FileService;
     private final FeedService feedService;
 
     @Transactional
