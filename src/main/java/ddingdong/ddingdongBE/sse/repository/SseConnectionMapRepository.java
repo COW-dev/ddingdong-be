fbbaf2c4c82fe92254dcae616bfb7469076abc72
5uhwann <5uhwann27@gmail.com>
2025-01-08 01:31:07 +0900

    feat: SseConnectionRepository 구현 - SseConnectionMapRepository

---
+++ src/main/java/ddingdong/ddingdongBE/sse/repository/SseConnectionMapRepository.java
@@ -0,0 +1,35 @@
+package ddingdong.ddingdongBE.sse.repository;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+public class SseConnectionMapRepository implements SseConnectionRepository {
+
+    private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+    @Override
+    public void save(String id, SseEmitter sseEmitter) {
+        emitters.put(id, sseEmitter);
+    }
+
+    @Override
+    public SseEmitter findById(String id) {
+        return emitters.get(id);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        SseEmitter removed = this.emitters.remove(id);
+        if (removed != null) {
+            removed.complete();
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        emitters.clear();
+    }
+}
---
+++ src/main/java/ddingdong/ddingdongBE/sse/repository/SseConnectionRepository.java
@@ -0,0 +1,15 @@
+package ddingdong.ddingdongBE.sse.repository;
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+public interface SseConnectionRepository {
+
+    void save(String id, SseEmitter sseEmitter);
+
+    SseEmitter findById(String id);
+
+    void deleteById(String id);
+
+    void deleteAll();
+
+}
