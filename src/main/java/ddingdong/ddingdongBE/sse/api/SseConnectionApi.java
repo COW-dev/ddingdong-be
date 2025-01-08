555f8dac679474f5df6752aaa89e650eaf48dee2
5uhwann <5uhwann27@gmail.com>
2025-01-08 21:17:05 +0900

    refactor: SseConnectionApi 명세 수정

--- src/main/java/ddingdong/ddingdongBE/sse/api/SseConnectionApi.java
+++ src/main/java/ddingdong/ddingdongBE/sse/api/SseConnectionApi.java
@@ -17,8 +17,13 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RequestMapping("/server/sse")
 public interface SseConnectionApi {
 
-    @Operation(summary = "SSE 구독")
-    @ApiResponse(responseCode = "200", description = "활동 보고서 전체 조회 성공")
+    @Operation(
+            summary = "SSE 구독",
+            description = "SSE 연결을 설정합니다. 연결 타임아웃은 60초입니다."
+    )
+    @ApiResponse(responseCode = "200", description = "SSE 구독 연결 성공")
+    @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자")
+    @ApiResponse(responseCode = "500", description = "서버 오류")
     @ResponseStatus(HttpStatus.OK)
     @SecurityRequirement(name = "AccessToken")
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
