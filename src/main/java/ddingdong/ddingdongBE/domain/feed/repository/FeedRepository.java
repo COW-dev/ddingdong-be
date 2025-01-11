78c3698e4eebe60386b7d5b22beaf48012777319
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-01-12 03:12:23 +0900

    refactor : 사용하지 않은 쿼리 제거

--- src/main/java/ddingdong/ddingdongBE/domain/feed/repository/FeedRepository.java
+++ src/main/java/ddingdong/ddingdongBE/domain/feed/repository/FeedRepository.java
@@ -1,26 +1,12 @@
 package ddingdong.ddingdongBE.domain.feed.repository;
 
 import ddingdong.ddingdongBE.domain.feed.entity.Feed;
-import java.util.List;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
-
-    @Query(value = """
-        SELECT * FROM feed f
-        WHERE f.id in
-        (SELECT max(id)
-        FROM feed
-        WHERE deleted_at IS NULL
-        GROUP BY club_id)
-        ORDER BY id DESC
-        """
-        , nativeQuery = true)
-    List<Feed> findNewestAll();
-
 
     @Query(value = """
         select *
