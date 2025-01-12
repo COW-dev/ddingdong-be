0022701a520b1c43fc829774ecf7150552169cd5
5uhwann <5uhwann27@gmail.com>
2025-01-13 00:15:38 +0900

    feat: VodProcessingNotificationAp i 구현

--- src/main/java/ddingdong/ddingdongBE/domain/vodprocessing/service/dto/command/UpdateAckedNotificationCommand.java
+++ src/main/java/ddingdong/ddingdongBE/domain/vodprocessing/service/dto/command/UpdateAckedNotificationCommand.java
@@ -1,6 +1,5 @@
 package ddingdong.ddingdongBE.domain.vodprocessing.service.dto.command;
 
-import ddingdong.ddingdongBE.domain.vodprocessing.entity.VodNotificationStatus;
 
 public record UpdateAckedNotificationCommand(
         Long vodNotificationId
