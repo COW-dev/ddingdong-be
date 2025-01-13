efe2c1d193de216865708996c1cd4768ad3bb108
5uhwann <106325839+5uhwann@users.noreply.github.com>
2025-01-13 21:05:10 +0900

    [DDING-000] validation 오류 수정 (#210)

--- src/main/java/ddingdong/ddingdongBE/domain/vodprocessing/controller/dto/request/CreatePendingVodProcessingJobRequest.java
+++ src/main/java/ddingdong/ddingdongBE/domain/vodprocessing/controller/dto/request/CreatePendingVodProcessingJobRequest.java
@@ -6,11 +6,10 @@ import org.hibernate.validator.constraints.UUID;
 
 public record CreatePendingVodProcessingJobRequest(
         @NotNull(message = "변환 작업 ID는 필수입니다")
-        @UUID
         String convertJobId,
         @NotNull(message = "userId는 필수입니다.")
         String userId,
-        @NotNull(message = "파일Id는 빌수입니다.")
+        @NotNull(message = "파일Id는 필수입니다.")
         @UUID
         String fileId
 ) {
