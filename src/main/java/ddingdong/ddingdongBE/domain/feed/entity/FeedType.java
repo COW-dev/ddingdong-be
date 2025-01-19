67e37881d4926342a2c0b955bfe414967e77122e
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-01-20 01:53:27 +0900

    fix : 사용하지 않는 로직 제거

--- src/main/java/ddingdong/ddingdongBE/domain/feed/entity/FeedType.java
+++ src/main/java/ddingdong/ddingdongBE/domain/feed/entity/FeedType.java
@@ -1,7 +1,6 @@
 package ddingdong.ddingdongBE.domain.feed.entity;
 
 import ddingdong.ddingdongBE.domain.filemetadata.entity.DomainType;
-import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -12,11 +11,4 @@ public enum FeedType {
   VIDEO(DomainType.FEED_VIDEO);
 
   private final DomainType domainType;
-
-  public static FeedType findByContentType(String contentType) {
-    return Arrays.stream(values())
-        .filter(feedType -> feedType.name().equalsIgnoreCase(contentType))
-        .findFirst()
-        .orElseThrow(() -> new IllegalArgumentException("Feed 내 해당 컨텐츠 종류(" + contentType + ")는 지원하지 않습니다."));
-  }
 }
