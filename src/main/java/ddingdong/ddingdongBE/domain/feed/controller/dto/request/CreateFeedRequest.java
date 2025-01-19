7b4cf623573913fd92b68ec5aff82ec91a252e04
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-01-19 21:28:15 +0900

    refactor : 피드 생성 시 mimeType으로 FeedType 정하도록 로직 수정

--- src/main/java/ddingdong/ddingdongBE/domain/feed/controller/dto/request/CreateFeedRequest.java
+++ src/main/java/ddingdong/ddingdongBE/domain/feed/controller/dto/request/CreateFeedRequest.java
@@ -12,16 +12,16 @@ public record CreateFeedRequest(
     @Schema(description = "이미지/비디오 식별자 id", example = "0192c828-ffce-7ee8-94a8-d9d4c8cdec00")
     @NotNull(message = "mediaId는 null이 될 수 없습니다.")
     String mediaId,
-    @Schema(description = "컨텐츠 종류", example = "IMAGE")
+    @Schema(description = "컨텐츠 종류", example = "image/png")
     @NotNull(message = "contentType은 null이 될 수 없습니다.")
-    String contentType
+    String mimeType
 ) {
 
     public CreateFeedCommand toCommand(User user) {
         return CreateFeedCommand.builder()
             .activityContent(activityContent)
             .mediaId(mediaId)
-            .contentType(contentType)
+            .mimeType(mimeType)
             .user(user)
             .build();
     }
