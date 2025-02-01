a9e4a7569c5b9c35844e14d513c7ba82f0bf0d10
ahyeungii <ahyeungii@gmail.com>
2025-02-02 06:09:35 +0900

    feat : 지원하기 스크립트 작성

---
+++ src/main/resources/db/migration/V35_create_form_response_and_answer_table.sql
@@ -0,0 +1,25 @@
+CREATE TABLE form_response
+(
+    id             BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    submitted_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    name           VARCHAR(50)  NOT NULL,
+    student_number VARCHAR(50)  NOT NULL,
+    status         VARCHAR(50)  NOT NULL,
+    form_id        BIGINT       NOT NULL,
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP NULL,
+    updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL,
+    CONSTRAINT fk_response_form FOREIGN KEY (form_id) REFERENCES form (id) ON DELETE CASCADE
+);
+
+CREATE TABLE form_answer
+(
+    id             BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    value          VARCHAR(255) NULL,
+    value_type     VARCHAR(50)  NOT NULL,
+    response_id    BIGINT       NOT NULL,
+    field_id       BIGINT       NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL,
+    CONSTRAINT fk_answer_response FOREIGN KEY (response_id) REFERENCES form_response (id) ON DELETE CASCADE,
+    CONSTRAINT fk_answer_field FOREIGN KEY (field_id) REFERENCES form_field (id) ON DELETE CASCADE
+);
