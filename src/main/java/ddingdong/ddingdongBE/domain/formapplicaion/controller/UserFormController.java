438292b9f8a444dcb047227c521b2c14247e7801
ahyeungii <ahyeungii@gmail.com>
2025-02-03 02:45:14 +0900

    fix: UserFormController에서 createFormResponse 호출

--- src/main/java/ddingdong/ddingdongBE/domain/formapplicaion/controller/UserFormController.java
+++ src/main/java/ddingdong/ddingdongBE/domain/formapplicaion/controller/UserFormController.java
@@ -2,6 +2,7 @@ package ddingdong.ddingdongBE.domain.formapplicaion.controller;
 
 import ddingdong.ddingdongBE.domain.formapplicaion.api.UserFormApi;
 import ddingdong.ddingdongBE.domain.formapplicaion.controller.dto.request.CreateFormResponseRequest;
+import ddingdong.ddingdongBE.domain.formapplicaion.service.FacadeUserFormService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserFormController implements UserFormApi {
 
+    private final FacadeUserFormService facadeUserFormService;
+
     @Override
-    public void createFormResponse(
-            CreateFormResponseRequest createFormResponseRequest
-    ) {
+    public void createFormResponse(CreateFormResponseRequest createFormResponseRequest) {
+
+        facadeUserFormService.createFormResponse(createFormResponseRequest.toCommand());
 
     }
 }
