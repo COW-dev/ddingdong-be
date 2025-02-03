57d02d9735563d679d9f0efc953ea6ccb7e199c2
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-02-04 04:15:48 +0900

    feat : 동아리 지원 폼지 전체 조회 API 구현

--- src/main/java/ddingdong/ddingdongBE/domain/form/repository/FormRepository.java
+++ src/main/java/ddingdong/ddingdongBE/domain/form/repository/FormRepository.java
@@ -1,8 +1,11 @@
 package ddingdong.ddingdongBE.domain.form.repository;
 
+import ddingdong.ddingdongBE.domain.club.entity.Club;
 import ddingdong.ddingdongBE.domain.form.entity.Form;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FormRepository extends JpaRepository<Form, Long> {
 
+    List<Form> findAllByClub(Club club);
 }
--- src/main/java/ddingdong/ddingdongBE/domain/form/service/FormService.java
+++ src/main/java/ddingdong/ddingdongBE/domain/form/service/FormService.java
@@ -1,6 +1,8 @@
 package ddingdong.ddingdongBE.domain.form.service;
 
+import ddingdong.ddingdongBE.domain.club.entity.Club;
 import ddingdong.ddingdongBE.domain.form.entity.Form;
+import java.util.List;
 
 public interface FormService {
 
@@ -9,4 +11,6 @@ public interface FormService {
     Form getById(Long formId);
 
     void delete(Form form);
+
+    List<Form> getAllByClub(Club club);
 }
