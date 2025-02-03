e450773d42593a1d987bd257ec8e3b67fa0b01f6
ahyeungii <ahyeungii@gmail.com>
2025-02-04 03:10:35 +0900

    feat: 지원자 전체 조회 API 구성

---
+++ src/main/java/ddingdong/ddingdongBE/domain/formapplicaion/api/CentralFormApi.java
@@ -0,0 +1,31 @@
+package ddingdong.ddingdongBE.domain.formapplicaion.api;
+
+import ddingdong.ddingdongBE.auth.PrincipalDetails;
+import ddingdong.ddingdongBE.domain.formapplicaion.controller.dto.response.MyFormApplicationPageResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Form - Club", description = "Form API")
+@RequestMapping("/server/central")
+public interface CentralFormApi {
+    @Operation(summary = "지원자 전체 조회 API")
+    @ApiResponse(responseCode = "200", description = "지원자 전체 조회 성공",
+            content = @Content(schema = @Schema(implementation = MyFormApplicationPageResponse.class)))
+    @ResponseStatus(HttpStatus.OK)
+    @SecurityRequirement(name = "AccessToken")
+    @GetMapping("/my/{formId}/applications")
+    MyFormApplicationPageResponse getMyFormApplicationPage(
+            @PathVariable("formId") Long formId,
+            @RequestParam(value = "size", defaultValue = "9") int size,
+            @RequestParam(value = "currentCursorId", defaultValue = "-1") Long currentCursorId,
+            @AuthenticationPrincipal PrincipalDetails principalDetails
+    );
+
+}
