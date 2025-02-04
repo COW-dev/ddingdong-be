a03061c373a65b00c53e8d064a0d5827abead8bf
ahyeungii <ahyeungii@gmail.com>
2025-02-05 03:02:01 +0900

    style: swagger 문서에 query parameter 설명 추가

--- src/main/java/ddingdong/ddingdongBE/domain/formapplication/api/CentralFormApplicationApi.java
+++ src/main/java/ddingdong/ddingdongBE/domain/formapplication/api/CentralFormApplicationApi.java
@@ -3,6 +3,7 @@ package ddingdong.ddingdongBE.domain.formapplication.api;
 import ddingdong.ddingdongBE.auth.PrincipalDetails;
 import ddingdong.ddingdongBE.domain.formapplication.controller.dto.response.MyFormApplicationPageResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/server/central")
 public interface CentralFormApplicationApi {
     @Operation(summary = "지원자 전체 조회 API")
+    @Parameter(name = "size", description = "한 페이지당 조회할 지원자 수 (기본값: 15)")
+    @Parameter(name = "currentCursorId", description = "현재 커서 위치 (첫 페이지: -1)")
     @ApiResponse(responseCode = "200", description = "지원자 전체 조회 성공",
             content = @Content(schema = @Schema(implementation = MyFormApplicationPageResponse.class)))
     @ResponseStatus(HttpStatus.OK)
