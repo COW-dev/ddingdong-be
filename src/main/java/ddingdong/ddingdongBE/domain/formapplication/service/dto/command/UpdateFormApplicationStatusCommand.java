58226d74ec9f76c1a084df72b94c7880f326c11e
ahyeungii <ahyeungii@gmail.com>
2025-02-07 04:26:03 +0900

    refactor: 불필요한 toEntity() 메서드 삭제

--- src/main/java/ddingdong/ddingdongBE/domain/formapplication/service/dto/command/UpdateFormApplicationStatusCommand.java
+++ src/main/java/ddingdong/ddingdongBE/domain/formapplication/service/dto/command/UpdateFormApplicationStatusCommand.java
@@ -1,7 +1,5 @@
 package ddingdong.ddingdongBE.domain.formapplication.service.dto.command;
 
-import ddingdong.ddingdongBE.domain.form.entity.Form;
-import ddingdong.ddingdongBE.domain.formapplication.entity.FormApplication;
 import ddingdong.ddingdongBE.domain.formapplication.entity.FormApplicationStatus;
 import ddingdong.ddingdongBE.domain.user.entity.User;
 import lombok.Builder;
@@ -15,10 +13,4 @@ public record UpdateFormApplicationStatusCommand(
         FormApplicationStatus status,
         User user
 ) {
-    public FormApplication toEntity(Form form) {
-        return FormApplication.builder()
-                .form(form)
-                .status(status)
-                .build();
-    }
 }
