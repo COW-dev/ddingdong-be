aa71b192f0fd579b3a365dd7b38690403194c0b1
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-02-11 01:12:10 +0900

    feat : Swagger 명세서 작성

--- src/main/java/ddingdong/ddingdongBE/domain/form/controller/dto/response/TextFieldStatisticsResponse.java
+++ src/main/java/ddingdong/ddingdongBE/domain/form/controller/dto/response/TextFieldStatisticsResponse.java
@@ -2,16 +2,23 @@ package ddingdong.ddingdongBE.domain.form.controller.dto.response;
 
 import ddingdong.ddingdongBE.domain.form.service.dto.query.TextFieldStatisticsQuery;
 import ddingdong.ddingdongBE.domain.form.service.dto.query.TextFieldStatisticsQuery.TextStatisticsQuery;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 public record TextFieldStatisticsResponse(
+        @Schema(description = "질문 유형", example = "CHECK_BOX")
         String type,
+        @ArraySchema(schema = @Schema(implementation = TextStatisticsResponse.class))
         List<TextStatisticsResponse> answers
 ) {
 
     record TextStatisticsResponse(
+            @Schema(description = "지원자 id", example = "1")
             Long applicationId,
+            @Schema(description = "지원자 이름", example = "고선제")
             String name,
+            @Schema(description = "지원자 답변", example = "답변입니다")
             String answer
     ) {
         public static TextStatisticsResponse from(TextStatisticsQuery query) {
