0595a3148c5a3c2238456e9421427b8ca8d7f5fe
koseonje <kosunje1344@naver.com>
2025-02-10 17:13:03 +0900

    [DDING-001] 지원자 전체 조회 및 상세조회 응답값 처리 수정 (#251)

--- src/main/java/ddingdong/ddingdongBE/domain/formapplication/service/dto/query/MyFormApplicationPageQuery.java
+++ src/main/java/ddingdong/ddingdongBE/domain/formapplication/service/dto/query/MyFormApplicationPageQuery.java
@@ -10,56 +10,56 @@ import java.util.Collections;
 import java.util.List;
 
 public record MyFormApplicationPageQuery(
-    String title,
-    LocalDate startDate,
-    LocalDate endDate,
-    boolean hasInterview,
-    List<FormApplicationListQuery> formApplicationListQueries,
-    PagingQuery pagingQuery
+        String title,
+        LocalDate startDate,
+        LocalDate endDate,
+        boolean hasInterview,
+        List<FormApplicationListQuery> formApplicationListQueries,
+        PagingQuery pagingQuery
 ) {
 
-  public static MyFormApplicationPageQuery of(Form form,
-      List<FormApplicationListQuery> formApplicationListQueries, PagingQuery pagingQuery) {
-    return new MyFormApplicationPageQuery(
-        form.getTitle(),
-        form.getStartDate(),
-        form.getEndDate(),
-        form.isHasInterview(),
-        formApplicationListQueries,
-        pagingQuery
-    );
-  }
-
-  public static MyFormApplicationPageQuery createEmpty() {
-    return new MyFormApplicationPageQuery(
-        "", // title
-        null, // startDate
-        null, // endDate
-        false, // hasInterview
-        Collections.emptyList(), // formApplicationListQueries
-        PagingQuery.createEmpty() // pagingQuery
-    );
-  }
-
-  @Builder
-  public record FormApplicationListQuery(
-      Long id,
-      Long formId,
-      LocalDateTime submittedAt,
-      String name,
-      String studentNumber,
-      String status
-  ) {
-
-    public static FormApplicationListQuery of(FormApplication formApplication) {
-      return FormApplicationListQuery.builder()
-          .id(formApplication.getId())
-          .formId(formApplication.getForm().getId())
-          .submittedAt(formApplication.getCreatedAt())
-          .name(formApplication.getName())
-          .studentNumber(formApplication.getStudentNumber())
-          .status(formApplication.getStatus().toString())
-          .build();
+    public static MyFormApplicationPageQuery of(Form form,
+            List<FormApplicationListQuery> formApplicationListQueries, PagingQuery pagingQuery) {
+        return new MyFormApplicationPageQuery(
+                form.getTitle(),
+                form.getStartDate(),
+                form.getEndDate(),
+                form.isHasInterview(),
+                formApplicationListQueries,
+                pagingQuery
+        );
     }
-  }
+
+    public static MyFormApplicationPageQuery createEmpty(Form form) {
+        return new MyFormApplicationPageQuery(
+                form.getTitle(), // title
+                form.getStartDate(), // startDate
+                form.getEndDate(), // endDate
+                form.isHasInterview(), // hasInterview
+                Collections.emptyList(), // formApplicationListQueries
+                PagingQuery.createEmpty() // pagingQuery
+        );
+    }
+
+    @Builder
+    public record FormApplicationListQuery(
+            Long id,
+            Long formId,
+            LocalDateTime submittedAt,
+            String name,
+            String studentNumber,
+            String status
+    ) {
+
+        public static FormApplicationListQuery of(FormApplication formApplication) {
+            return FormApplicationListQuery.builder()
+                    .id(formApplication.getId())
+                    .formId(formApplication.getForm().getId())
+                    .submittedAt(formApplication.getCreatedAt())
+                    .name(formApplication.getName())
+                    .studentNumber(formApplication.getStudentNumber())
+                    .status(formApplication.getStatus().toString())
+                    .build();
+        }
+    }
 }
