eac921a0f376eb66f1f8a1e416eb347fa80c8d06
Seooooo24 <ahyeungii@gmail.com>
2025-02-14 01:51:52 +0900

    [DDING-113] 지원자 수 조회 API 구현 (#263)

--- src/main/java/ddingdong/ddingdongBE/domain/formapplication/api/UserFormApplicationApi.java
+++ src/main/java/ddingdong/ddingdongBE/domain/formapplication/api/UserFormApplicationApi.java
@@ -1,11 +1,16 @@
 package ddingdong.ddingdongBE.domain.formapplication.api;
 
+import ddingdong.ddingdongBE.auth.PrincipalDetails;
 import ddingdong.ddingdongBE.domain.formapplication.controller.dto.request.CreateFormApplicationRequest;
+import ddingdong.ddingdongBE.domain.formapplication.controller.dto.response.FormApplicationCountResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Form - User", description = "User Form API")
@@ -19,6 +24,16 @@ public interface UserFormApplicationApi {
     void createFormApplication(
             @PathVariable Long formId,
             @Valid @RequestBody CreateFormApplicationRequest request
+    );
+
+    @Operation(summary = "지원자 수 조회 API")
+    @ApiResponse(responseCode = "200", description = "지원자 수 조회 성공",
+            content = @Content(schema = @Schema(implementation = FormApplicationCountResponse.class)))
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/forms/{formId}/applications/count")
+    FormApplicationCountResponse getNumberOfFormApplication(
+            @PathVariable("formId") Long formId,
+            @AuthenticationPrincipal PrincipalDetails principalDetails
     );
 
 }
