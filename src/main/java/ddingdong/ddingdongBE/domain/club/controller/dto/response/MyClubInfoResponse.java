6c5e01e17d5a06afeda2ada1938176ee805486d2
ahyeungii <ahyeungii@gmail.com>
2025-02-14 03:12:31 +0900

    refactor: Central 동아리 상세조회 응답 dto formId 삭제, startDate endDate 추가

--- src/main/java/ddingdong/ddingdongBE/domain/club/controller/dto/response/MyClubInfoResponse.java
+++ src/main/java/ddingdong/ddingdongBE/domain/club/controller/dto/response/MyClubInfoResponse.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import ddingdong.ddingdongBE.domain.club.service.dto.query.MyClubInfoQuery;
 import ddingdong.ddingdongBE.file.service.dto.query.UploadedFileUrlQuery;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Schema(
         name = "MyClubInfoResponse",
@@ -25,10 +25,10 @@ public record MyClubInfoResponse(
         String location,
         @Schema(description = "모집시작날짜", example = "2024-01-01 00:00")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-        LocalDateTime startRecruitPeriod,
+        LocalDate startDate,
         @Schema(description = "모집마감날짜", example = "2024-01-01 00:00")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-        LocalDateTime endRecruitPeriod,
+        LocalDate endDate,
         @Schema(description = "정기활동", example = "정기활동")
         String regularMeeting,
         @Schema(description = "동아리 소개", example = "소개")
@@ -37,8 +37,6 @@ public record MyClubInfoResponse(
         String activity,
         @Schema(description = "인재상", example = "인재상")
         String ideal,
-        @Schema(description = "모집 폼 ID", example = "6")
-        Long formId,
         @Schema(description = "동아리 프로필 이미지 Url", example = "url")
         MyClubInfoImageUrlResponse profileImage,
         MyClubInfoImageUrlResponse introductionImage
@@ -52,13 +50,12 @@ public record MyClubInfoResponse(
                 query.leader(),
                 query.phoneNumber(),
                 query.location(),
-                query.startRecruitPeriod(),
-                query.endRecruitPeriod(),
+                query.startDate(),
+                query.endDate(),
                 query.regularMeeting(),
                 query.introduction(),
                 query.activity(),
                 query.ideal(),
-                query.formId(),
                 MyClubInfoImageUrlResponse.from(query.profileImageUrlQuery()),
                 MyClubInfoImageUrlResponse.from(query.introductionImageUrlQuery())
         );
