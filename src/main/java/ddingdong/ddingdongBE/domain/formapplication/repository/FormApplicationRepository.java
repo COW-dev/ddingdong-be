edc8851221d917a00299cef21f933d3940933b67
5uhwann <5uhwann27@gmail.com>
2025-02-17 01:13:52 +0900

    refactor: 불필요 코드 제거

--- src/main/java/ddingdong/ddingdongBE/domain/formapplication/repository/FormApplicationRepository.java
+++ src/main/java/ddingdong/ddingdongBE/domain/formapplication/repository/FormApplicationRepository.java
@@ -7,7 +7,6 @@ import ddingdong.ddingdongBE.domain.formapplication.repository.dto.DepartmentInf
 import ddingdong.ddingdongBE.domain.formapplication.repository.dto.RecentFormInfo;
 import java.time.LocalDate;
 import java.util.List;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,20 +14,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FormApplicationRepository extends JpaRepository<FormApplication, Long> {
-
-    @Query(value = """
-            select *
-              from form_application f
-              where (:currentCursorId = -1 or id < :currentCursorId)
-              and f.form_id = :formId
-              order by f.id DESC
-              limit :size
-            """, nativeQuery = true)
-    Slice<FormApplication> findPageByFormIdOrderById(
-            @Param("formId") Long formId,
-            @Param("size") int size,
-            @Param("currentCursorId") Long currentCursorId
-    );
 
     Long countByForm(Form form);
 
