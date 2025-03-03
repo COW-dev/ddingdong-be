9cb229210defc14c80d9ba25557625bdfa25db40
wonjunYou <youwjune@gmail.com>
2025-03-03 22:12:29 +0900

    [DDING-120] 시작 일자(start_date) 인덱스 추가

--- src/main/resources/db/migration/V42__alter_table_add_index_activity_report.sql
+++ src/main/resources/db/migration/V42__alter_table_add_index_activity_report.sql
@@ -5,3 +5,7 @@ CREATE INDEX activity_report_term_index
 CREATE INDEX activity_report_created_at_index
     ON ddingdong_dev.activity_report (created_at)
     COMMENT '활동보고서 생성 일자(created_at) 인덱스';
+
+CREATE INDEX activity_report_start_date_index
+    ON ddingdong_dev.activity_report (start_date)
+    COMMENT '활동보고서 시작 일자(start_date) 인덱스';
