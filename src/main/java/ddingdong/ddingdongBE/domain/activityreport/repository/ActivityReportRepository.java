3f8c2d688ca645010c8f72abef5aeb973ffd340b
5uhwann <5uhwann27@gmail.com>
2025-03-04 20:35:26 +0900

    feat: 현재 년도 및 회차에 해당하는 활동보고서 조회 쿼리 작성

--- src/main/java/ddingdong/ddingdongBE/domain/activityreport/repository/ActivityReportRepository.java
+++ src/main/java/ddingdong/ddingdongBE/domain/activityreport/repository/ActivityReportRepository.java
@@ -5,10 +5,21 @@ import ddingdong.ddingdongBE.domain.activityreport.entity.ActivityReport;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ActivityReportRepository extends JpaRepository<ActivityReport, Long> {
+
+    @Query(value = """
+        select ac from ActivityReport ac
+        join fetch ac.club c
+        where YEAR(ac.createdAt) = :currentYear
+        and ac.term = :term
+        and c.deletedAt is null
+        """)
+    List<ActivityReport> findAllByCurrentYearAndTerm(@Param("currentYear") int currentYear, @Param("term") int term);
 
     List<ActivityReport> findByClubNameAndTerm(String clubName, String term);
 
