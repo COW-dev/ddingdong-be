858b8b25f92679f0bc8c1126539919f28109d41b
koseonje <kosunje1344@naver.com>
2025-03-05 08:26:12 +0900

    [DDING-000] 사용자 동아리 전체 목록 조회시 쿼리 대량 발생 수정 (#287)

--- src/main/java/ddingdong/ddingdongBE/domain/club/service/ClubService.java
+++ src/main/java/ddingdong/ddingdongBE/domain/club/service/ClubService.java
@@ -1,6 +1,7 @@
 package ddingdong.ddingdongBE.domain.club.service;
 
 import ddingdong.ddingdongBE.domain.club.entity.Club;
+import ddingdong.ddingdongBE.domain.club.repository.dto.UserClubListInfo;
 import java.util.List;
 
 public interface ClubService {
@@ -18,4 +19,6 @@ public interface ClubService {
     void delete(Long clubId);
 
     Club getByUserIdWithFetch(Long userId);
+
+    List<UserClubListInfo> findAllClubListInfo();
 }
