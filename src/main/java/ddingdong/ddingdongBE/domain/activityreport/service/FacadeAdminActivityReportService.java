e2e03a13b1918e2f192a0c0f99af9d85559d6409
5uhwann <5uhwann27@gmail.com>
2025-03-05 23:25:44 +0900

    refactor: 어드민 - 활동보고서 상세 조회 API 구현

--- src/main/java/ddingdong/ddingdongBE/domain/activityreport/service/FacadeAdminActivityReportService.java
+++ src/main/java/ddingdong/ddingdongBE/domain/activityreport/service/FacadeAdminActivityReportService.java
@@ -1,6 +1,7 @@
 package ddingdong.ddingdongBE.domain.activityreport.service;
 
 import ddingdong.ddingdongBE.domain.activityreport.service.dto.command.CreateActivityTermInfoCommand;
+import ddingdong.ddingdongBE.domain.activityreport.service.dto.query.ActivityReportQuery;
 import ddingdong.ddingdongBE.domain.activityreport.service.dto.query.AdminActivityReportListQuery;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -8,6 +9,8 @@ import java.util.List;
 public interface FacadeAdminActivityReportService {
 
     List<AdminActivityReportListQuery> getActivityReports(LocalDateTime now, int term);
+
+    List<ActivityReportQuery> getActivityReport(Long clubId, LocalDateTime now, int term);
 
     void createActivityTermInfo(CreateActivityTermInfoCommand command);
 }
--- src/main/java/ddingdong/ddingdongBE/domain/activityreport/service/FacadeAdminActivityReportServiceImpl.java
+++ src/main/java/ddingdong/ddingdongBE/domain/activityreport/service/FacadeAdminActivityReportServiceImpl.java
@@ -3,7 +3,14 @@ package ddingdong.ddingdongBE.domain.activityreport.service;
 import ddingdong.ddingdongBE.domain.activityreport.entity.ActivityReport;
 import ddingdong.ddingdongBE.domain.activityreport.service.dto.command.CreateActivityTermInfoCommand;
 import ddingdong.ddingdongBE.domain.activityreport.service.dto.query.ActivityReportInfo;
+import ddingdong.ddingdongBE.domain.activityreport.service.dto.query.ActivityReportQuery;
 import ddingdong.ddingdongBE.domain.activityreport.service.dto.query.AdminActivityReportListQuery;
+import ddingdong.ddingdongBE.domain.club.entity.Club;
+import ddingdong.ddingdongBE.domain.club.service.ClubService;
+import ddingdong.ddingdongBE.domain.filemetadata.entity.DomainType;
+import ddingdong.ddingdongBE.domain.filemetadata.service.FileMetaDataService;
+import ddingdong.ddingdongBE.file.service.S3FileService;
+import ddingdong.ddingdongBE.file.service.dto.query.UploadedFileUrlQuery;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -17,8 +24,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class FacadeAdminActivityReportServiceImpl implements FacadeAdminActivityReportService {
 
+    private final ClubService clubService;
     private final ActivityReportTermInfoService activityReportTermInfoService;
     private final ActivityReportService activityReportService;
+    private final FileMetaDataService fileMetaDataService;
+    private final S3FileService s3FileService;
 
     @Override
     public List<AdminActivityReportListQuery> getActivityReports(LocalDateTime now, int term) {
@@ -27,10 +37,31 @@ public class FacadeAdminActivityReportServiceImpl implements FacadeAdminActivity
         return parseToListQuery(activityReports);
     }
 
+    @Override
+    public List<ActivityReportQuery> getActivityReport(Long clubId, LocalDateTime now, int term) {
+        Club club = clubService.getById(clubId);
+        int currentYear = now.getYear();
+        List<ActivityReport> activityReports = activityReportService.getActivityReport(club, currentYear, term);
+
+        return activityReports.stream()
+                .map(this::parseToQuery)
+                .toList();
+    }
+
     @Transactional
     @Override
     public void createActivityTermInfo(CreateActivityTermInfoCommand command) {
         activityReportTermInfoService.create(command.startDate(), command.totalTermCount());
+    }
+
+    private ActivityReportQuery parseToQuery(ActivityReport activityReport) {
+        UploadedFileUrlQuery image = fileMetaDataService
+                .getCoupledAllByDomainTypeAndEntityId(DomainType.ACTIVITY_REPORT_IMAGE, activityReport.getId())
+                .stream()
+                .map(fileMetaData -> s3FileService.getUploadedFileUrl(fileMetaData.getFileKey()))
+                .findFirst()
+                .orElse(null);
+        return ActivityReportQuery.of(activityReport, image);
     }
 
     private List<AdminActivityReportListQuery> parseToListQuery(final List<ActivityReport> activityReports) {
