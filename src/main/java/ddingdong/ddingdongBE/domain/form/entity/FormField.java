b0a7156b3488082a619261f98772cfa6ee043c04
5uhwann <5uhwann27@gmail.com>
2025-03-08 00:17:04 +0900

    refactor: 코드 스타일 수정

--- src/main/java/ddingdong/ddingdongBE/domain/form/entity/FormField.java
+++ src/main/java/ddingdong/ddingdongBE/domain/form/entity/FormField.java
@@ -23,61 +23,61 @@ import lombok.NoArgsConstructor;
 @Getter
 public class FormField extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(nullable = false)
-  private String question;
+    @Column(nullable = false)
+    private String question;
 
-  @Column(nullable = false)
-  private boolean required;
+    @Column(nullable = false)
+    private boolean required;
 
-  @Column(nullable = false)
-  private int fieldOrder;
+    @Column(nullable = false)
+    private int fieldOrder;
 
-  @Column(nullable = false)
-  private String section;
+    @Column(nullable = false)
+    private String section;
 
-  @Convert(converter = StringListConverter.class)
-  private List<String> options;
+    @Convert(converter = StringListConverter.class)
+    private List<String> options;
 
-  @Enumerated(EnumType.STRING)
-  @Column(nullable = false)
-  private FieldType fieldType;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FieldType fieldType;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  private Form form;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Form form;
 
-  @Builder
-  private FormField(
-      String question,
-      FieldType fieldType,
-      boolean required,
-      int fieldOrder,
-      String section,
-      List<String> options,
-      Form form
-  ) {
-    this.question = question;
-    this.fieldType = fieldType;
-    this.required = required;
-    this.fieldOrder = fieldOrder;
-    this.section = section;
-    this.options = options;
-    this.form = form;
-  }
+    @Builder
+    private FormField(
+            String question,
+            FieldType fieldType,
+            boolean required,
+            int fieldOrder,
+            String section,
+            List<String> options,
+            Form form
+            ) {
+        this.question = question;
+        this.fieldType = fieldType;
+        this.required = required;
+        this.fieldOrder = fieldOrder;
+        this.section = section;
+        this.options = options;
+        this.form = form;
+    }
 
-  public boolean isMultipleChoice() {
-    return this.fieldType == FieldType.CHECK_BOX || this.fieldType == FieldType.RADIO;
-  }
+    public boolean isMultipleChoice() {
+        return this.fieldType == FieldType.CHECK_BOX || this.fieldType == FieldType.RADIO;
+    }
 
-  public boolean isTextType() {
-    return this.fieldType == FieldType.TEXT || this.fieldType == FieldType.LONG_TEXT
-            || this.fieldType == FieldType.FILE;
-  }
+    public boolean isTextType() {
+        return this.fieldType == FieldType.TEXT || this.fieldType == FieldType.LONG_TEXT
+                || this.fieldType == FieldType.FILE;
+    }
 
-  public boolean isFile() {
-    return this.fieldType == FieldType.FILE;
-  }
+    public boolean isFile() {
+        return this.fieldType == FieldType.FILE;
+    }
 }
