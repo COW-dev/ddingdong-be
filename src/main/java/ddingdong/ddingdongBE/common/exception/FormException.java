253c161d43f1c428567cc5b559c3fa9e8e0a35a8
ahyeungii <ahyeungii@gmail.com>
2025-03-08 15:18:05 +0900

    refactor: 코멘트 반영

--- src/main/java/ddingdong/ddingdongBE/common/exception/FormException.java
+++ src/main/java/ddingdong/ddingdongBE/common/exception/FormException.java
@@ -5,7 +5,7 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 public class FormException extends CustomException {
 
     public static final String FORM_PERIOD_ERROR_MESSAGE = "해당 폼지의 응답기간이 아닙니다";
-    private static final String OVERLAP_FORM_DATE_MESSAGE = "입력한 기간과 겹치는 폼이 존재합니다.";
+    private static final String OVERLAPPED_FORM_DATE_MESSAGE = "입력한 기간과 겹치는 폼이 존재합니다.";
     private static final String INVALID_FIELD_TYPE_MESSAGE = "통계를 조회할 질문 유형이 올바르지 않습니다.";
     private static final String INVALID_FORM_DATE_MESSAGE = "폼지 종료일자는 폼지 시작일자 이전일 수 없습니다.";
 
@@ -24,7 +24,7 @@ public class FormException extends CustomException {
     public static final class OverlapFormPeriodException extends FormException {
 
         public OverlapFormPeriodException() {
-            super(OVERLAP_FORM_DATE_MESSAGE, BAD_REQUEST.value());
+            super(OVERLAPPED_FORM_DATE_MESSAGE, BAD_REQUEST.value());
         }
     }
 
--- src/main/java/ddingdong/ddingdongBE/domain/form/controller/dto/request/UpdateFormEndDateRequest.java
+++ src/main/java/ddingdong/ddingdongBE/domain/form/controller/dto/request/UpdateFormEndDateRequest.java
@@ -3,10 +3,14 @@ package ddingdong.ddingdongBE.domain.form.controller.dto.request;
 import ddingdong.ddingdongBE.domain.form.service.dto.command.UpdateFormEndDateCommand;
 import ddingdong.ddingdongBE.domain.user.entity.User;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 
 public record UpdateFormEndDateRequest (
         @Schema(description = "폼지 종료일자", example = "2025-03-10")
+        @NotNull(message = "폼지 종료일자는 null이 될 수 없습니다.")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         LocalDate endDate
 ){
     public UpdateFormEndDateCommand toCommand(User user, Long formId) {
