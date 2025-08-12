87eb6b2223c1014628fa140ea3dc095f9cadf023
koseonje <kosunje1344@naver.com>
2025-08-13 02:35:23 +0900

    [DDING-133] 지원자 동아리 명단 연동시 기존 인원 초기화 로직 구현 (#312)
    (cherry picked from commit e0735098ca38d64ee7be1cd71480bcbd4176f43e)

--- src/test/java/ddingdong/ddingdongBE/common/fixture/FormFixture.java
+++ src/test/java/ddingdong/ddingdongBE/common/fixture/FormFixture.java
@@ -1,13 +1,27 @@
 package ddingdong.ddingdongBE.common.fixture;
 
+import ddingdong.ddingdongBE.domain.club.entity.Club;
 import ddingdong.ddingdongBE.domain.form.entity.FieldType;
 import ddingdong.ddingdongBE.domain.form.entity.Form;
 import ddingdong.ddingdongBE.domain.form.entity.FormField;
 import ddingdong.ddingdongBE.domain.formapplication.entity.FormAnswer;
+import ddingdong.ddingdongBE.domain.formapplication.entity.FormApplication;
+import ddingdong.ddingdongBE.domain.formapplication.entity.FormApplicationStatus;
 import java.time.LocalDate;
 import java.util.List;
 
 public class FormFixture {
+
+    public static Form createForm(Club club) {
+        return Form.builder()
+                .title("모집 지원서")
+                .description("동아리 모집을 위한 지원서입니다.")
+                .startDate(LocalDate.of(2025, 4, 1))
+                .endDate(LocalDate.of(2025, 4, 15))
+                .hasInterview(true)
+                .sections(List.of("자기소개", "지원 동기", "경력 및 경험"))
+                .build();
+    }
 
     public static Form formWithClubNull() {
         return Form.builder()
@@ -36,6 +50,18 @@ public class FormFixture {
         return FormAnswer.builder()
                 .value(List.of("답변"))
                 .formField(savedFormField)
+                .build();
+    }
+
+    public static FormApplication createFormApplicationFinalPass(Form form) {
+        return FormApplication.builder()
+                .name("김철수")
+                .studentNumber("60191234")
+                .department("컴퓨터공학과")
+                .phoneNumber("010-1234-5678")
+                .email("chulsoo.kim@mju.ac.kr")
+                .status(FormApplicationStatus.FINAL_PASS) // 또는 적절한 enum 값
+                .form(form) // 기존에 생성된 Form 객체
                 .build();
     }
 }
