a0f80e080021fe4ae4d5d416e621ca0a46074361
koseonje <kosunje1344@naver.com>
2025-10-01 02:02:19 +0900

    [DDING-000] Swagger version 명시 (#331)

--- src/main/java/ddingdong/ddingdongBE/common/config/OpenApiConfig.java
+++ src/main/java/ddingdong/ddingdongBE/common/config/OpenApiConfig.java
@@ -18,6 +18,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI()
+                .openapi("3.0.1")
                 .components(securityComponents())
                 .servers(List.of(new Server().url(serverUrl)));
     }
