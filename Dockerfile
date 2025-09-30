16ad1360bc271a08a916eb87cb3ada596a69a6e1
koseonje <kosunje1344@naver.com>
2025-09-30 21:47:43 +0900

    [DDING-000] 환경변수 자동 설정 스크립트 작성 (#329)
    (cherry picked from commit 8dc55a1191d647d18f5183e7b364574717763f04)

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,6 @@ RUN ./gradlew clean build -x test --no-daemon
 
 # Package stage
 FROM openjdk:17
-COPY otel/opentelemetry-javaagent.jar /otel/opentelemetry-javaagent.jar
 COPY --from=build /home/gradle/src/build/libs/*.jar /app.jar
 EXPOSE 8080
-ENTRYPOINT ["java", \
-    "-javaagent:/otel/opentelemetry-javaagent.jar", \
-    "-jar", "/app.jar"]
+ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/ddingdong/ddingdongBE/common/runner/PromtailDockerComposeRunner.java
+++ src/main/java/ddingdong/ddingdongBE/common/runner/PromtailDockerComposeRunner.java
@@ -19,7 +19,13 @@ public class PromtailDockerComposeRunner implements ApplicationRunner {
         log.info("Running PromtailDockerComposeRunner");
 
         ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("docker-compose", "-f", "/var/app/current/promtail/promtail-docker-compose.yml", "up", "-d");
+        processBuilder.command(
+                "docker-compose",
+                "-f",
+                "/var/app/current/promtail/promtail-docker-compose.yml",
+                "--env-file", "/var/app/current/.env",
+                "up", "-d"
+        );
 
         Process process = processBuilder.start();
 
