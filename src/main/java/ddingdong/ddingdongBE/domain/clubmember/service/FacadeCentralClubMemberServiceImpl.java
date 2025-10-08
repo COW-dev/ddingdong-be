bca28cbc3e464a557bf59b8977cae98fccc7516b
koseonje <kosunje1344@naver.com>
2025-10-08 15:16:23 +0900

    [DDING-000] 동아리원 명단 업로드 로직 수정 (#333)
    (cherry picked from commit c632a35b2d1f53fc229e753b587051aba5c14db1)

--- src/main/java/ddingdong/ddingdongBE/domain/clubmember/service/FacadeCentralClubMemberServiceImpl.java
+++ src/main/java/ddingdong/ddingdongBE/domain/clubmember/service/FacadeCentralClubMemberServiceImpl.java
@@ -8,10 +8,7 @@ import ddingdong.ddingdongBE.domain.clubmember.service.dto.command.UpdateClubMem
 import ddingdong.ddingdongBE.domain.clubmember.service.dto.command.UpdateClubMemberListCommand;
 import ddingdong.ddingdongBE.domain.clubmember.service.dto.query.AllClubMemberInfoQuery;
 import ddingdong.ddingdongBE.file.service.ExcelFileService;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -43,22 +40,10 @@ public class FacadeCentralClubMemberServiceImpl implements FacadeCentralClubMemb
     @Transactional
     public void updateMemberList(UpdateClubMemberListCommand command) {
         Club club = clubService.getByUserId(command.userId());
-        List<ClubMember> updateClubMemberInfos =
+        List<ClubMember> updateClubMember =
                 excelFileService.extractClubMembersInformation(club, command.clubMemberListFile());
-        List<ClubMember> clubMembers = clubMemberService.getByClubId(club.getId());
-        Set<Long> updatedMemberInfoIds = updateClubMemberInfos.stream()
-                .map(ClubMember::getId)
-                .collect(Collectors.toSet());
-        Set<Long> currentMemberIds = clubMembers.stream()
-                .map(ClubMember::getId)
-                .collect(Collectors.toSet());
-
-        clubMemberService.saveAll(filterCreatedMembers(updateClubMemberInfos, updatedMemberInfoIds, currentMemberIds));
-        clubMemberService.updateAll(filterUpdatedMembers(updateClubMemberInfos, updatedMemberInfoIds, currentMemberIds));
-
-        List<ClubMember> deletedMembers = filterDeletedMembers(clubMembers, updatedMemberInfoIds, currentMemberIds);
-        club.removeAll(deletedMembers);
-        clubMemberService.deleteAll(deletedMembers);
+        clubMemberService.deleteAllByClubId(club.getId());
+        clubMemberService.saveAll(updateClubMember);
     }
 
     @Override
@@ -84,33 +69,5 @@ public class FacadeCentralClubMemberServiceImpl implements FacadeCentralClubMemb
         ClubMember clubMember = command.toEntity(club);
         clubMemberService.save(clubMember);
         club.addClubMember(clubMember);
-    }
-
-    private List<ClubMember> filterCreatedMembers(List<ClubMember> updatedClubMembers, Set<Long> updatedMemberIds,
-                                                  Set<Long> currentMemberIds) {
-        Set<Long> createdMemberIds = new HashSet<>(updatedMemberIds);
-        createdMemberIds.removeAll(currentMemberIds);
-        return updatedClubMembers.stream()
-                .filter(member -> createdMemberIds.contains(member.getId()))
-                .toList();
-    }
-
-    private List<ClubMember> filterUpdatedMembers(List<ClubMember> updatedClubMembers, Set<Long> updatedMemberIds,
-            Set<Long> currentMemberIds) {
-        Set<Long> willUpdateMemberIds = new HashSet<>(currentMemberIds);
-        willUpdateMemberIds.retainAll(updatedMemberIds);
-        return updatedClubMembers.stream()
-                .filter(member -> willUpdateMemberIds.contains(member.getId()))
-                .toList();
-    }
-
-    private List<ClubMember> filterDeletedMembers(List<ClubMember> clubMembers, Set<Long> updatedMemberIds,
-                                                  Set<Long> currentMemberIds) {
-        Set<Long> deletedMemberIds = new HashSet<>(currentMemberIds);
-        deletedMemberIds.removeAll(updatedMemberIds);
-        System.out.println(deletedMemberIds);
-        return clubMembers.stream()
-                .filter(member -> deletedMemberIds.contains(member.getId()))
-                .toList();
     }
 }
