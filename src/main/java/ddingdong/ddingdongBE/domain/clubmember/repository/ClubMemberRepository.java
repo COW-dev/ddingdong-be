fde3190b06bab62d21a36615f430e349c125b0cc
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-10-08 13:09:45 +0900

    fix: 동아리원 명단 업로드 로직 수정

--- src/main/java/ddingdong/ddingdongBE/domain/clubmember/repository/ClubMemberRepository.java
+++ src/main/java/ddingdong/ddingdongBE/domain/clubmember/repository/ClubMemberRepository.java
@@ -4,10 +4,16 @@ import ddingdong.ddingdongBE.domain.clubmember.entity.ClubMember;
 import java.util.Collection;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
 
     List<ClubMember> findByIdIn(Collection<Long> ids);
 
     List<ClubMember> findByClubId(Long clubId);
+
+    @Modifying
+    @Query("UPDATE ClubMember cm SET cm.deletedAt = CURRENT_TIMESTAMP WHERE cm.club.id = :clubId AND cm.deletedAt IS NULL")
+    void deleteAllByClubId(Long clubId);
 }
