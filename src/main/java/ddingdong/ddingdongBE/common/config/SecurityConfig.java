fc92e826a23defa9d519ed36efa0ca241234b654
koseonje <kosunje1344@naver.com>
2025-11-01 17:53:41 +0900

    [DDING-000] SSE 비동기 전송 시 Spring Security AccessDeniedException 발생 문제 해결 (#337)
    (cherry picked from commit 3da599b803197889239fc08758a6afa63a39d652)

--- src/main/java/ddingdong/ddingdongBE/common/config/SecurityConfig.java
+++ src/main/java/ddingdong/ddingdongBE/common/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import ddingdong.ddingdongBE.auth.service.JwtAuthService;
 import ddingdong.ddingdongBE.common.filter.JwtAuthenticationFilter;
 import ddingdong.ddingdongBE.common.handler.CustomAccessDeniedHandler;
 import ddingdong.ddingdongBE.common.handler.RestAuthenticationEntryPoint;
+import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -32,6 +33,7 @@ public class SecurityConfig {
             throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .dispatcherTypeMatchers(DispatcherType.ASYNC).permitAll()
                         .requestMatchers(API_PREFIX + "/auth/**",
                                 API_PREFIX + "/events/**")
                         .permitAll()
