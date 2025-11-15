65e3177945a263088011891df5d673814b8e5a65
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-11-15 19:22:02 +0900

    feat: 어노테이션 수정

--- src/main/java/ddingdong/ddingdongBE/domain/formapplication/entity/EmailSendHistory.java
+++ src/main/java/ddingdong/ddingdongBE/domain/formapplication/entity/EmailSendHistory.java
@@ -17,7 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
-import javax.annotation.Nullable;
+import jakarta.annotation.Nullable;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
