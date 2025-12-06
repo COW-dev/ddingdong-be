2eb1a3138f59f4c33a4176e683a1997864959035
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-12-06 18:28:25 +0900

    feat: 스레드 풀 분리

--- src/main/java/ddingdong/ddingdongBE/common/config/AsyncConfig.java
+++ src/main/java/ddingdong/ddingdongBE/common/config/AsyncConfig.java
@@ -11,11 +11,22 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 public class AsyncConfig {
 
     @Bean
+    public Executor generalAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean
     public Executor emailAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(10);
         executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(500);
+        executor.setQueueCapacity(5000);
         executor.setThreadNamePrefix("EmailAsync-");
         executor.initialize();
         return executor;
