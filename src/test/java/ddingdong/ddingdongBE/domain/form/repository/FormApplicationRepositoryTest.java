ca655d644788eee8ad2bfd64d4d441180c262e42
Seooooo24 <ahyeungii@gmail.com>
2026-01-02 04:54:04 +0900

    test: findAllFinalPassedByFormId 쿼리 검증

--- src/test/java/ddingdong/ddingdongBE/domain/form/repository/FormApplicationRepositoryTest.java
+++ src/test/java/ddingdong/ddingdongBE/domain/form/repository/FormApplicationRepositoryTest.java
@@ -20,12 +20,10 @@ import ddingdong.ddingdongBE.domain.user.repository.UserRepository;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
-import org.hibernate.mapping.DependantBasicValue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Slice;
 
 class FormApplicationRepositoryTest extends DataJpaTestSupport {
 
@@ -40,8 +38,6 @@ class FormApplicationRepositoryTest extends DataJpaTestSupport {
 
     @Autowired
     private FormApplicationRepository formApplicationRepository;
-
-    private final FixtureMonkey fixture = FixtureMonkeyFactory.getNotNullBuilderIntrospectorMonkey();
 
     @BeforeEach
     void setUp() {
@@ -104,18 +100,6 @@ class FormApplicationRepositoryTest extends DataJpaTestSupport {
         // 2위 검증
         assertThat(result.get(1).getDepartment()).isEqualTo("디지털콘텐츠디자인학과");
         assertThat(result.get(1).getCount()).isEqualTo(2L);
-    }
-
-    private FormApplication createApp(Form form, String department) {
-        return FormApplication.builder()
-                .name("지원자")
-                .studentNumber("60000000")
-                .phoneNumber("010-0000-000")
-                .email("email@email.com")
-                .department(department)
-                .status(FormApplicationStatus.SUBMITTED)
-                .form(form)
-                .build();
     }
 
     @DisplayName("주어진 날짜를 기준으로 주어진 개수만큼 최신 폼지의 시작일과 지원 수를 반환한다.")
@@ -213,150 +197,75 @@ class FormApplicationRepositoryTest extends DataJpaTestSupport {
         assertThat(recentFormInfos.get(1).getCount()).isEqualTo(2);
         assertThat(recentFormInfos.get(1).getDate()).isEqualTo(LocalDate.of(2020, 3, 1));
     }
+
+    @DisplayName("특정 동아리 폼지의 모든 최종 합격자 리스트를 반환한다.")
+    @Test
+    void findAllFinalPassedByFormId() {
+        // given
+        User user = User.builder()
+                .name("유저")
+                .role(Role.CLUB)
+                .build();
+        User savedUser = userRepository.save(user);
+
+        Club club = Club.builder()
+                .name("동아리")
+                .user(savedUser)
+                .score(Score.from(BigDecimal.ZERO))
+                .build();
+        Club savedClub = clubRepository.save(club);
+
+        Form form = Form.builder()
+                .title("폼지")
+                .description("설명")
+                .hasInterview(false)
+                .club(savedClub)
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now())
+                .sections(List.of("섹션1"))
+                .build();
+        Form savedForm = formRepository.save(form);
+
+        List<FormApplication> applications = List.of(
+                createApp(savedForm, FormApplicationStatus.FINAL_PASS),
+                createApp(savedForm, FormApplicationStatus.FINAL_PASS),
+                createApp(savedForm, FormApplicationStatus.FINAL_PASS),
+                createApp(savedForm, FormApplicationStatus.FIRST_PASS),
+                createApp(savedForm, FormApplicationStatus.FINAL_FAIL),
+                createApp(savedForm, FormApplicationStatus.FINAL_FAIL),
+                createApp(savedForm, FormApplicationStatus.SUBMITTED)
+        );
+        formApplicationRepository.saveAll(applications);
+
+        // when
+        formApplicationRepository.findAllFinalPassedByFormId(savedForm.getId());
+
+        // then
+        assertThat(formApplicationRepository.findAllFinalPassedByFormId(savedForm.getId())
+                .size()).isEqualTo(4);
+    }
+
+    private FormApplication createApp(Form form, String department) {
+        return FormApplication.builder()
+                .name("지원자")
+                .studentNumber("60000000")
+                .phoneNumber("010-0000-000")
+                .email("email@email.com")
+                .department(department)
+                .status(FormApplicationStatus.SUBMITTED)
+                .form(form)
+                .build();
+    }
+
+    private FormApplication createApp(Form form, FormApplicationStatus status) {
+        return FormApplication.builder()
+                .name("지원자")
+                .studentNumber("60000000")
+                .phoneNumber("010-0000-000")
+                .email("email@email.com")
+                .department("학과1")
+                .status(status)
+                .form(form)
+                .build();
+    }
 }
-//    @DisplayName("지원자 수 상위 5개 학과와 그 개수를 반환한다.")
-//    @Test
-//    void findTopFiveDepartmentsByForm_ShouldReturnTopFiveDepartments() throws InterruptedException {
-//        // given
-//        Form form = fixture.giveMeBuilder(Form.class)
-//                .set("club", null)
-//                .set("sections", List.of("공통"))
-//                .sample();
-//        Form savedForm = formRepository.save(form);
-//        FormApplication formApplication = FormApplication.builder()
-//                .name("이름1")
-//                .studentNumber("학번1")
-//                .department("학과1")
-//                .status(FormApplicationStatus.SUBMITTED)
-//                .form(savedForm)
-//                .build();
-//        FormApplication formApplication2 = FormApplication.builder()
-//                .name("이름1")
-//                .studentNumber("학번1")
-//                .department("학과2")
-//                .status(FormApplicationStatus.SUBMITTED)
-//                .form(savedForm)
-//                .build();
-//        FormApplication formApplication3 = FormApplication.builder()
-//                .name("이름1")
-//                .studentNumber("학번1")
-//                .department("학과2")
-//                .status(FormApplicationStatus.SUBMITTED)
-//                .form(savedForm)
-//                .build();
-//        FormApplication formApplication4 = FormApplication.builder()
-//                .name("이름1")
-//                .studentNumber("학번1")
-//                .department("학과3")
-//                .status(FormApplicationStatus.SUBMITTED)
-//                .form(savedForm)
-//                .build();
-//        FormApplication formApplication5 = FormApplication.builder()
-//                .name("이름1")
-//                .studentNumber("학번1")
-//                .department("학과3")
-//                .status(FormApplicationStatus.SUBMITTED)
-//                .form(savedForm)
-//                .build();
-//        FormApplication formApplication6 = FormApplication.builder()
-//                .name("이름1")
-//                .studentNumber("학번1")
-//                .department("학과3")
-//                .status(FormApplicationStatus.SUBMITTED)
-//                .form(savedForm)
-//                .build();
-//
-//        formApplicationRepository.saveAll(
-//                List.of(formApplication, formApplication2, formApplication3, formApplication4, formApplication5,
-//                        formApplication6)
-//        );
-//        // when
-//        List<DepartmentInfo> topFive = formApplicationRepository.findTopDepartmentsByFormId(savedForm.getId(),5);
-//        // then
-//
-//        assertThat(topFive.size()).isEqualTo(3);
-//        assertThat(topFive.get(0).getCount()).isEqualTo(3);
-//        assertThat(topFive.get(0).getDepartment()).isEqualTo("학과3");
-//        assertThat(topFive.get(1).getCount()).isEqualTo(2);
-//        assertThat(topFive.get(1).getDepartment()).isEqualTo("학과2");
-//        assertThat(topFive.get(2).getCount()).isEqualTo(1);
-//        assertThat(topFive.get(2).getDepartment()).isEqualTo("학과1");
-//    }
-//
-//    @DisplayName("주어진 날짜를 기준으로 주어진 개수만큼 최신 폼지의 시작일과 지원 수를 반환한다.")
-//    @Test
-//    void findRecentFormByDateWithApplicationCount() {
-//        // given
-//        Club club1 = fixture.giveMeBuilder(Club.class)
-//                .set("id", 1L)
-//                .set("user", null)
-//                .set("score", Score.from(BigDecimal.ZERO))
-//                .set("clubMembers", null)
-//                .sample();
-//        Club savedClub = clubRepository.save(club1);
-//        Form form = fixture.giveMeBuilder(Form.class)
-//                .set("id", 1L)
-//                .set("club", savedClub)
-//                .set("startDate", LocalDate.of(2020, 3, 1))
-//                .set("endDate", LocalDate.of(2020, 4, 1))
-//                .set("sections", List.of("공통"))
-//                .sample();
-//        Form savedForm = formRepository.save(form);
-//        FormApplication formApplication = FormApplication.builder()
-//                .name("이름1")
-//                .studentNumber("학번1")
-//                .department("학과1")
-//                .status(FormApplicationStatus.SUBMITTED)
-//                .form(savedForm)
-//                .build();
-//        FormApplication formApplication2 = FormApplication.builder()
-//                .name("이름1")
-//                .studentNumber("학번1")
-//                .department("학과1")
-//                .status(FormApplicationStatus.SUBMITTED)
-//                .form(savedForm)
-//                .build();
-//
-//        formApplicationRepository.saveAll(List.of(formApplication, formApplication2));
-//
-//        Form form2 = fixture.giveMeBuilder(Form.class)
-//                .set("id", 2L)
-//                .set("club", savedClub)
-//                .set("startDate", LocalDate.of(2020, 1, 1))
-//                .set("endDate", LocalDate.of(2020, 2, 1))
-//                .set("sections", List.of("공통"))
-//                .sample();
-//        Form savedForm2 = formRepository.save(form2);
-//        FormApplication formApplication3 = FormApplication.builder()
-//                .name("이름1")
-//                .studentNumber("학번1")
-//                .department("학과1")
-//                .status(FormApplicationStatus.SUBMITTED)
-//                .form(savedForm2)
-//                .build();
-//        formApplicationRepository.save(formApplication3);
-//
-//        Form form3 = fixture.giveMeBuilder(Form.class)
-//                .set("id", 1L)
-//                .set("club", savedClub)
-//                .set("startDate", LocalDate.of(2020, 5, 1))
-//                .set("endDate", LocalDate.of(2020, 6, 1))
-//                .set("sections", List.of("공통"))
-//                .sample();
-//        formRepository.save(form3);
-//        // when
-//        List<RecentFormInfo> recentFormInfos = formApplicationRepository.findRecentFormByDateWithApplicationCount(
-//                savedClub.getId(),
-//                savedForm.getEndDate(),
-//                3
-//        );
-//        // then
-//        assertThat(recentFormInfos.size()).isEqualTo(2);
-//        assertThat(recentFormInfos.get(0).getCount()).isEqualTo(1);
-//        assertThat(recentFormInfos.get(0).getDate()).isEqualTo(LocalDate.of(2020, 1, 1));
-//        assertThat(recentFormInfos.get(1).getCount()).isEqualTo(2);
-//        assertThat(recentFormInfos.get(1).getDate()).isEqualTo(LocalDate.of(2020, 3, 1));
-//
-//
-//    }
-//}
