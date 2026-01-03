0e57b03fdd8015caa59b4adcb0c98770ba0b691e
Seooooo24 <ahyeungii@gmail.com>
2026-01-03 23:05:09 +0900

    test: FormAnswerFixture, FormFieldFixture 생성

---
+++ src/test/java/ddingdong/ddingdongBE/common/fixture/FormFieldFixture.java
@@ -0,0 +1,29 @@
+package ddingdong.ddingdongBE.common.fixture;
+
+import ddingdong.ddingdongBE.domain.form.entity.FieldType;
+import ddingdong.ddingdongBE.domain.form.entity.Form;
+import ddingdong.ddingdongBE.domain.form.entity.FormField;
+import java.util.List;
+
+public class FormFieldFixture {
+
+    public static FormField create(Form form) {
+        return FormField.builder()
+                .section("섹션")
+                .fieldType(FieldType.RADIO)
+                .form(form)
+                .question("질문")
+                .options(List.of("선택지"))
+                .build();
+    }
+
+    public static FormField create(Form form, String question) {
+        return FormField.builder()
+                .section("섹션")
+                .fieldType(FieldType.RADIO)
+                .form(form)
+                .question(question)
+                .options(List.of("선택지"))
+                .build();
+    }
+}
