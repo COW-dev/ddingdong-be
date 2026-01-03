26f9ec9f4553ad18d839e1e86622170bd83a5f32
Seooooo24 <ahyeungii@gmail.com>
2026-01-03 23:22:29 +0900

    refactor: FileMetaDataFixture 메소드 추가

--- src/test/java/ddingdong/ddingdongBE/common/fixture/FileMetaDataFixture.java
+++ src/test/java/ddingdong/ddingdongBE/common/fixture/FileMetaDataFixture.java
@@ -7,12 +7,23 @@ import java.util.UUID;
 
 public class FileMetaDataFixture {
 
-    public static FileMetaData formFileMetaData(UUID id, Long entityId) {
+    public static FileMetaData create(UUID id, Long entityId) {
         return FileMetaData.builder()
                 .id(id)
                 .fileKey("1")
                 .fileStatus(FileStatus.COUPLED)
                 .fileName("파일")
+                .domainType(DomainType.FORM_FILE)
+                .entityId(entityId)
+                .build();
+    }
+
+    public static FileMetaData create(UUID id, Long entityId, String filename) {
+        return FileMetaData.builder()
+                .id(id)
+                .fileKey("1")
+                .fileStatus(FileStatus.COUPLED)
+                .fileName(filename)
                 .domainType(DomainType.FORM_FILE)
                 .entityId(entityId)
                 .build();
