226ccb8fe2b4ebb5671a9ce9fcc190c3295fb6e5
koseonje <kosunje1344@naver.com>
2026-01-06 22:44:42 +0900

    [DDING-000] revert & feed 전체 조회 로직 수정 (#357)
    (cherry picked from commit 207b7f71bd905ba07f2a97e281bba656f58aa2b5)

--- src/main/java/ddingdong/ddingdongBE/domain/feed/controller/dto/response/FeedPageResponse.java
+++ src/main/java/ddingdong/ddingdongBE/domain/feed/controller/dto/response/FeedPageResponse.java
@@ -1,31 +1,31 @@
 package ddingdong.ddingdongBE.domain.feed.controller.dto.response;
 
 import ddingdong.ddingdongBE.domain.feed.service.dto.query.FeedListQuery;
-import ddingdong.ddingdongBE.domain.feed.service.dto.query.NewestFeedPerClubPageQuery;
+import ddingdong.ddingdongBE.domain.feed.service.dto.query.FeedPageQuery;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.Builder;
 
-public record NewestFeedPerClubPageResponse(
-        @ArraySchema(schema = @Schema(name = "동아리 최신 피드 정보", implementation = NewestFeedListResponse.class))
-        List<NewestFeedListResponse> newestFeeds,
+public record FeedPageResponse(
+        @ArraySchema(schema = @Schema(name = "동아리 최신 피드 정보", implementation = FeedListResponse.class))
+        List<FeedListResponse> newestFeeds,
         @Schema(name = "피드 페이지 정보", implementation = PagingResponse.class)
         PagingResponse pagingInfo
 ) {
 
-    public static NewestFeedPerClubPageResponse from(
-            NewestFeedPerClubPageQuery newestFeedPerClubPageQuery) {
-        List<NewestFeedListResponse> newestFeeds = newestFeedPerClubPageQuery.feedListQueries()
+    public static FeedPageResponse from(
+            FeedPageQuery feedPageQuery) {
+        List<FeedListResponse> newestFeeds = feedPageQuery.feedListQueries()
                 .stream()
-                .map(NewestFeedListResponse::from)
+                .map(FeedListResponse::from)
                 .toList();
-        return new NewestFeedPerClubPageResponse(newestFeeds,
-                PagingResponse.from(newestFeedPerClubPageQuery.pagingQuery()));
+        return new FeedPageResponse(newestFeeds,
+                PagingResponse.from(feedPageQuery.pagingQuery()));
     }
 
     @Builder
-    public record NewestFeedListResponse(
+    public record FeedListResponse(
             @Schema(description = "피드 ID", example = "1")
             Long id,
             @Schema(description = "피드 썸네일 CDN URL", example = "https://%s.s3.%s.amazonaws.com/%s/%s/%s")
@@ -38,8 +38,8 @@ public record NewestFeedPerClubPageResponse(
             String feedType
     ) {
 
-        public static NewestFeedListResponse from(FeedListQuery query) {
-            return NewestFeedListResponse.builder()
+        public static FeedListResponse from(FeedListQuery query) {
+            return FeedListResponse.builder()
                     .id(query.id())
                     .thumbnailOriginUrl(query.thumbnailOriginUrl())
                     .thumbnailCdnUrl(query.thumbnailCdnUrl())
