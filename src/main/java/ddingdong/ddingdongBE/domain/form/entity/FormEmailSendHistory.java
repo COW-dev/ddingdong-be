de019cc7fa83343290176ea2d0f136fa2c9bccba
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2026-01-19 19:42:10 +0900

    feat: script에 baseEntity 필드 추가

--- src/main/java/ddingdong/ddingdongBE/domain/form/entity/FormEmailSendHistory.java
+++ src/main/java/ddingdong/ddingdongBE/domain/form/entity/FormEmailSendHistory.java
@@ -1,5 +1,6 @@
 package ddingdong.ddingdongBE.domain.form.entity;
 
+import ddingdong.ddingdongBE.common.BaseEntity;
 import ddingdong.ddingdongBE.domain.formapplication.entity.FormApplicationStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -18,7 +19,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class FormEmailSendHistory {
+public class FormEmailSendHistory extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
