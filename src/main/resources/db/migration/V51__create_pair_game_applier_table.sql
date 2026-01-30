b20c6d814507b24811a6914e521b5e5a55a477c3
Seooooo24 <ahyeungii@gmail.com>
2026-01-30 15:18:03 +0900

    chore: pairGameApplier 테이블 생성 flyway 스크립트 작성

---
+++ src/main/resources/db/migration/V51__create_pair_game_applier_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE pair_game_applier
+(
+    id                       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name                     VARCHAR(50)  NOT NULL,
+    department               VARCHAR(50)  NOT NULL,
+    student_number           VARCHAR(50)  NOT NULL,
+    phone_number             VARCHAR(50)  NOT NULL,
+    student_fee_image_url    VARCHAR(255) NOT NULL,
+    created_at               TIMESTAMP DEFAULT CURRENT_TIMESTAMP NULL,
+    updated_at               TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL
+    deleted_at               TIMESTAMP NULL DEFAULT NULL
+);
