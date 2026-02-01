05f4df81ebe7d60e8338e04fdb2cf1e0ff344542
Seooooo24 <ahyeungii@gmail.com>
2026-02-01 20:03:48 +0900

    test: FacadeUserPairGameServiceTest 통합 테스트 작성

---
+++ src/test/java/ddingdong/ddingdongBE/domain/pairgame/service/FacadeUserPairGameServiceTest.java
@@ -0,0 +1,107 @@
+package ddingdong.ddingdongBE.domain.pairgame.service;
+
+import ddingdong.ddingdongBE.common.exception.FileException.UploadedFileNotFoundException;
+import ddingdong.ddingdongBE.common.support.TestContainerSupport;
+import ddingdong.ddingdongBE.domain.filemetadata.entity.DomainType;
+import ddingdong.ddingdongBE.domain.filemetadata.entity.FileMetaData;
+import ddingdong.ddingdongBE.domain.filemetadata.entity.FileStatus;
+import ddingdong.ddingdongBE.domain.filemetadata.repository.FileMetaDataRepository;
+import ddingdong.ddingdongBE.domain.pairgame.entity.PairGameApplier;
+import ddingdong.ddingdongBE.domain.pairgame.repository.PairGameRepository;
+import ddingdong.ddingdongBE.domain.pairgame.service.dto.command.CreatePairGameApplierCommand;
+import ddingdong.ddingdongBE.domain.pairgame.service.dto.query.PairGameMetaDataQuery;
+import ddingdong.ddingdongBE.file.service.S3FileService;
+import ddingdong.ddingdongBE.file.service.dto.query.UploadedFileUrlQuery;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+
+@SpringBootTest
+class FacadeUserPairGameServiceTest extends TestContainerSupport {
+
+    @Autowired
+    private FacadeUserPairGameService facadeUserPairGameService;
+
+    @Autowired
+    private PairGameRepository pairGameRepository;
+
+    @Autowired
+    private FileMetaDataRepository fileMetaDataRepository;
+
+    @MockitoBean
+    private S3FileService s3FileService;
+
+    @Test
+    @DisplayName("유저: 학번 중복 검사 후 응모자를 생성할 수 있다.")
+    void createPairGameApplier_Success() {
+        // given
+        MockMultipartFile file = new MockMultipartFile("file", "test.jpg", "image/jpeg", "content".getBytes());
+        CreatePairGameApplierCommand command = new CreatePairGameApplierCommand(
+                "김띵동", "융합소프트웨어학부", "60112233", "010-1234-5678", file
+        );
+        given(s3FileService.uploadMultipartFile(eq(file), any(LocalDateTime.class), eq("pair-game")))
+                .willReturn("s3-uploaded-file-key");
+
+        // when
+        facadeUserPairGameService.createPairGameApplier(command);
+
+        // then
+        List<PairGameApplier> appliers = pairGameRepository.findAll();
+        assertThat(appliers).hasSize(1);
+
+        PairGameApplier savedApplier = appliers.get(0);
+        assertThat(savedApplier.getName()).isEqualTo("김띵동");
+        assertThat(savedApplier.getStudentNumber()).isEqualTo("60112233");
+    }
+
+    @Test
+    @DisplayName("유저: 응모자 생성 요청에 파일이 없으면 예외가 발생한다.")
+    void createPairGameApplier_Fail_NoFile() {
+        // given
+        CreatePairGameApplierCommand command = new CreatePairGameApplierCommand(
+                "김띵동", "융합소프트웨어학부", "60112233", "010-1234-5678", null
+        );
+
+        // when & then
+        assertThatThrownBy(() -> facadeUserPairGameService.createPairGameApplier(command))
+                .isInstanceOf(UploadedFileNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("유저: 동아리 로고 이미지를 랜덤으로 18개 조회할 수 있다.")
+    void getPairGameMetaData_Integration() {
+        // given
+        List<FileMetaData> metaDataList = IntStream.range(0, 20)
+                .mapToObj(i -> FileMetaData.builder()
+                        .domainType(DomainType.CLUB_PROFILE)
+                        .fileKey("key" + i)
+                        .fileName("test.jpg")
+                        .fileStatus(FileStatus.COUPLED)
+                        .build())
+                .toList();
+        fileMetaDataRepository.saveAll(metaDataList);
+
+        given(s3FileService.getUploadedFileUrl(any()))
+                .willReturn(new UploadedFileUrlQuery("id", "originUrl", "cdnUrl"));
+
+        // when
+        PairGameMetaDataQuery result = facadeUserPairGameService.getPairGameMetaData();
+
+        // then
+        assertThat(result.images()).hasSize(18);
+        assertThat(result.images().get(0)).isEqualTo("cdnUrl");
+    }
+}
